758acfa4e14dbc48094d4497daae555bccffa519
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-05 14:59:35 -0700

    Spoons Devs

---
+++ Hungry-Developers/Hungry-Developers/Develepor.swift
@@ -0,0 +1,28 @@
+//
+//  Develepor.swift
+//  Hungry-Developers
+//
+//  Created by Hector Steven on 6/5/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import Foundation
+
+class Developer {
+	let leftSpoon = Spoon()
+	let rightSpoon = Spoon()
+	
+	func think() {
+		//both spoons are held so think for a time
+	}
+	
+	func eat() {
+		
+		// pause for a random amout of time then put spoons down
+	}
+	
+	func run() {
+		
+	}
+	
+}

523f8977055a25150eeff5c2d2e50bd20c2ac9af
Christopher Aronson <christopheraronson@icloud.com>
2019-06-05 13:33:17 -0700

    Init Commit

---
+++ HungryDevelopers/HungryDevelopers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HungryDevelopers
+//
+//  Created by Christopher Aronson on 6/5/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

e70bfd31f29858da36ea78d8fa8adb6e7964937f
hectorsvill <hectorstevenvillasano@gmail.com>
2019-06-05 15:34:09 -0700

    think and eat with timer.init

--- Hungry-Developers/Hungry-Developers/Develepor.swift
+++ Hungry-Developers/Hungry-Developers/Develepor.swift
@@ -12,17 +12,36 @@ class Developer {
 	let leftSpoon = Spoon()
 	let rightSpoon = Spoon()
 	
+	let timerRange = 0.01...0.053455
+	
 	func think() {
 		//both spoons are held so think for a time
+		
+		self.leftSpoon.pickUp()
+		self.rightSpoon.pickUp()
+		let ran = Double.random(in: timerRange)
+		
+		let _ = Timer.init(timeInterval: ran, repeats: true) { (_) in
+			self.eat()
+		}
 	}
 	
 	func eat() {
-		
 		// pause for a random amout of time then put spoons down
+		//putDown()
+		let ran = Double.random(in: timerRange)
+		
+		let _ = Timer.init(timeInterval: ran, repeats: true) { _ in
+			self.leftSpoon.putDown()
+			self.rightSpoon.putDown()
+		}
 	}
 	
 	func run() {
+		//should call think() then eat() over and over again forever.
 		
+		think()
+		eat()
 	}
 	
 }

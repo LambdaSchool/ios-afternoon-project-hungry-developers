8224d4055b559bc8c38e287c74383f38ace56e70
Kobe McKee <kobe.mckee@gmail.com>
2019-06-12 13:46:51 -0600

    Finished Product. Everyone gets to eat

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -31,11 +31,12 @@ class Spoon {
 class Developer {
     
     var exists = true
-    
+    let name: String
     var leftSpoon: Spoon?
     var rightSpoon: Spoon?
     
-    init(leftSpoon: Spoon? = nil, rightSpoon: Spoon? = nil) {
+    init(name: String, leftSpoon: Spoon? = nil, rightSpoon: Spoon? = nil) {
+        self.name = name
         self.leftSpoon = leftSpoon
         self.rightSpoon = rightSpoon
     }
@@ -43,11 +44,13 @@ class Developer {
    
     func think() {
         leftSpoon?.pickUp()
+        print("\(self.name) attempting to pick up left Spoon")
         rightSpoon?.pickUp()
+        print("\(self.name) attempting to pick up right Spoon")
     }
     
     func eat() {
-        let duration = UInt32.random(in: 10_000..<100_000)
+        let duration = UInt32.random(in: 10..<100)
         usleep(duration)
         leftSpoon?.putDown()
         rightSpoon?.putDown()
@@ -67,7 +70,7 @@ class Developer {
 
 
 
-func Dinner() {
+func dinner() {
     
     let spoon1 = Spoon(isInHand: false)
     let spoon2 = Spoon(isInHand: false)
@@ -76,17 +79,20 @@ func Dinner() {
     let spoon5 = Spoon(isInHand: false)
     
     
-    let dev1 = Developer(leftSpoon: spoon1, rightSpoon: spoon5)
-    let dev2 = Developer(leftSpoon: spoon2, rightSpoon: spoon1)
-    let dev3 = Developer(leftSpoon: spoon3, rightSpoon: spoon2)
-    let dev4 = Developer(leftSpoon: spoon4, rightSpoon: spoon3)
-    let dev5 = Developer(leftSpoon: spoon5, rightSpoon: spoon4)
+    let dev1 = Developer(name: "dev1", leftSpoon: spoon1, rightSpoon: spoon5)
+    let dev2 = Developer(name: "dev2", leftSpoon: spoon2, rightSpoon: spoon1)
+    let dev3 = Developer(name: "dev3", leftSpoon: spoon3, rightSpoon: spoon2)
+    let dev4 = Developer(name: "dev4", leftSpoon: spoon4, rightSpoon: spoon3)
+    let dev5 = Developer(name: "dev5", leftSpoon: spoon5, rightSpoon: spoon4)
     
     let developers: [Developer] = [dev1, dev2, dev3, dev4, dev5]
     
-    DispatchQueue.concurrentPerform(iterations: 5) { _ in
+    DispatchQueue.concurrentPerform(iterations: 5) {
+        print("Starting")
         developers[$0].run()
     }
     
     
 }
+
+dinner()

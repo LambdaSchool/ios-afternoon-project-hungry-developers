0fa25de27480961829c6cd28997231c121cebc75
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-12 15:22:28 -0600

    Scenario runs, need to debug.
    No way of checking who is picking up/putting down what spoon or who is eating. Add name property to Dev class.

--- Hungry Developers/Hungry Developers/HungryDev.swift
+++ Hungry Developers/Hungry Developers/HungryDev.swift
@@ -22,76 +22,79 @@ class ViewController: UIViewController {
         let dev3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
         let dev4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5)
         let dev5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1)
-        let developers = [dev1, dev2, dev3, dev4, dev5]
+        let developers: [Developer] = [dev1, dev2, dev3, dev4, dev5]
         
         DispatchQueue.concurrentPerform(iterations: 5) {
-            developers[$0].run
+            developers[$0].run()
         }
     }
     
-}
-
-class Spoon {
-    
-    let lock = NSLock()
-    var beingUsed: Bool = false
-    
-    func pickUp() {
-        lock.lock()
-        beingUsed = true
-        print("spoon was picked up")
-    }
-    
-    func putDown() {
-        lock.unlock()
-        beingUsed = false
-        print("spoon was put down")
-    }
-}
-
-
-class Developer {
-    
-    var leftSpoon: Spoon
-    var rightSpoon: Spoon
-    var hasLeft: Bool
-    var hasRight: Bool
-    var runScenario: Bool = true
-    
-    init(leftSpoon: Spoon, rightSpoon: Spoon) {
-        self.leftSpoon = leftSpoon
-        self.rightSpoon = rightSpoon
-    }
-    
-    func think() {
-        print("developer is thinking")
-        if leftSpoon.beingUsed == false {
-            leftSpoon.pickUp()
-            hasLeft = true
-        }
-        if rightSpoon.beingUsed == false {
-            rightSpoon.pickUp()
-            hasRight = true
-        }
-        if hasLeft == true && hasRight == true {
-            eat()
+    class Spoon {
+        
+        let lock = NSLock()
+        var beingUsed: Bool = false
+        
+        func pickUp() {
+            lock.lock()
+            beingUsed = true
+            print("spoon was picked up")
+            lock.unlock()
         }
         
+        func putDown() {
+            lock.lock()
+            beingUsed = false
+            print("spoon was put down")
+            lock.unlock()
+        }
     }
     
-    func eat() {
-        print("developer is eating")
-        usleep(100)
-        leftSpoon.putDown()
-        rightSpoon.putDown()
-    }
     
-    func run() {
-        print("running")
-        if runScenario {
-            for developer in developers {
-                think()
+    class Developer {
+        
+        var leftSpoon: Spoon
+        var rightSpoon: Spoon
+        var hasLeft: Bool
+        var hasRight: Bool
+        var runScenario: Bool = true
+        
+        init(leftSpoon: Spoon, rightSpoon: Spoon, hasLeft: Bool = false, hasRight: Bool = true) {
+            self.leftSpoon = leftSpoon
+            self.rightSpoon = rightSpoon
+            self.hasLeft = hasLeft
+            self.hasRight = hasRight
+        }
+        
+        func think() {
+            print("developer is thinking")
+            if leftSpoon.beingUsed == false {
+                leftSpoon.pickUp()
+                hasLeft = true
+            }
+            if rightSpoon.beingUsed == false {
+                rightSpoon.pickUp()
+                hasRight = true
+            }
+            if hasLeft == true && hasRight == true {
                 eat()
+            }
+            
+        }
+        
+        func eat() {
+            print("developer is eating")
+            usleep(100)
+            leftSpoon.putDown()
+            rightSpoon.putDown()
+        }
+        
+        func run() {
+            print("running")
+            if runScenario {
+                while runScenario {
+                    think()
+                    eat()
+                }
             }
         }
     }

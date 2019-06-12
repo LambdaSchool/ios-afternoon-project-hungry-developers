e6d3257b940b2252d21d18e372d3fd1922eef35c
Sameera Leola <pixelandbyte@gmail.com>
2019-06-12 18:00:24 -0500

    Working

---
+++ HungryDevelopers/HungryDevelopers/Spoons.swift
@@ -0,0 +1,24 @@
+//
+//  Spoons.swift
+//  HungryDevelopers
+//
+//  Created by Sameera Roussi on 6/12/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import Foundation
+
+class Spoons {
+    
+    let developer = Developers()
+    
+    var spoons: [Bool] = [true, true, true, true, true]
+
+    func pickUp() {
+        // Give the spoon to the developer
+    }
+    
+    func putDown() {
+        // Put the spoon back
+    }
+}
---
+++ scratch/scratch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  scratch
+//
+//  Created by Sameera Roussi on 6/12/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import Foundation
+
+var spoons: UInt8 = 0b00000000
+

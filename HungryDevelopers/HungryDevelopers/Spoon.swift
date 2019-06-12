56ba55693d483fb74a683792fc00e026dd7c2199
Sameera Leola <pixelandbyte@gmail.com>
2019-06-12 15:32:56 -0500

    Initial commit

---
+++ HungryDevelopers/HungryDevelopers/Spoon.swift
@@ -0,0 +1,22 @@
+//
+//  Spoon.swift
+//  HungryDevelopers
+//
+//  Created by Sameera Roussi on 6/12/19.
+//  Copyright © 2019 Sameera Roussi. All rights reserved.
+//
+
+import Foundation
+
+class Spoon {
+    
+    func pickUp() {
+        
+    }
+    
+    // Put down the spoon
+    func putDown() {
+        
+    }
+    
+} // end class

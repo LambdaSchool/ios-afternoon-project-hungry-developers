1bea910afbf3f888d99d9945ffa861dc41ecfb5d
Mitchell Budge <mitchellbudge@gmail.com>
2019-06-12 15:59:02 -0600

    Added names and print statements to identify actions

--- Hungry Developers/Hungry Developers/HungryDev.swift
+++ Hungry Developers/Hungry Developers/HungryDev.swift
@@ -11,17 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
-        let spoon1 = Spoon()
-        let spoon2 = Spoon()
-        let spoon3 = Spoon()
-        let spoon4 = Spoon()
-        let spoon5 = Spoon()
+        let spoon1 = Spoon(index: 1)
+        let spoon2 = Spoon(index: 2)
+        let spoon3 = Spoon(index: 3)
+        let spoon4 = Spoon(index: 4)
+        let spoon5 = Spoon(index: 5)
         
-        let dev1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2)
-        let dev2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3)
-        let dev3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
-        let dev4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5)
-        let dev5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1)
+        let dev1 = Developer(name: "Mitch", leftSpoon: spoon1, rightSpoon: spoon2)
+        let dev2 = Developer(name: "Kobe", leftSpoon: spoon2, rightSpoon: spoon3)
+        let dev3 = Developer(name: "Nelson", leftSpoon: spoon3, rightSpoon: spoon4)
+        let dev4 = Developer(name: "Hayden", leftSpoon: spoon4, rightSpoon: spoon5)
+        let dev5 = Developer(name: "Julian", leftSpoon: spoon5, rightSpoon: spoon1)
         let developers: [Developer] = [dev1, dev2, dev3, dev4, dev5]
         
         DispatchQueue.concurrentPerform(iterations: 5) {
@@ -33,18 +33,21 @@ class ViewController: UIViewController {
         
         let lock = NSLock()
         var beingUsed: Bool = false
+        var index: Int
+        
+        init(index: Int) {
+            self.index = index
+        }
         
         func pickUp() {
             lock.lock()
             beingUsed = true
-            print("spoon was picked up")
             lock.unlock()
         }
         
         func putDown() {
             lock.lock()
             beingUsed = false
-            print("spoon was put down")
             lock.unlock()
         }
     }
@@ -52,13 +55,15 @@ class ViewController: UIViewController {
     
     class Developer {
         
+        var name: String
         var leftSpoon: Spoon
         var rightSpoon: Spoon
         var hasLeft: Bool
         var hasRight: Bool
         var runScenario: Bool = true
         
-        init(leftSpoon: Spoon, rightSpoon: Spoon, hasLeft: Bool = false, hasRight: Bool = true) {
+        init(name: String, leftSpoon: Spoon, rightSpoon: Spoon, hasLeft: Bool = false, hasRight: Bool = true) {
+            self.name = name
             self.leftSpoon = leftSpoon
             self.rightSpoon = rightSpoon
             self.hasLeft = hasLeft
@@ -66,26 +71,31 @@ class ViewController: UIViewController {
         }
         
         func think() {
-            print("developer is thinking")
-            if leftSpoon.beingUsed == false {
+            print("\(name) is thinking")
+            if leftSpoon.beingUsed == false && leftSpoon.index < rightSpoon.index {
                 leftSpoon.pickUp()
+                print("\(name) picked up his left spoon")
                 hasLeft = true
-            }
-            if rightSpoon.beingUsed == false {
-                rightSpoon.pickUp()
-                hasRight = true
+                if rightSpoon.beingUsed == false && rightSpoon.index < leftSpoon.index {
+                    rightSpoon.pickUp()
+                    print("\(name) picked up his right spoon")
+                    hasRight = true
+                }
             }
             if hasLeft == true && hasRight == true {
+                print("\(name) has both of his spoons and is about to eat.")
                 eat()
             }
             
         }
         
         func eat() {
-            print("developer is eating")
+            print("\(name) is eating")
             usleep(100)
             leftSpoon.putDown()
+            print("\(name) put down their left spoon")
             rightSpoon.putDown()
+            print("\(name) put down their right spoon")
         }
         
         func run() {

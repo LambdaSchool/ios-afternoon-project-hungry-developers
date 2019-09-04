cb8a829fa63f455dd061f21692c60c469b99420a
Marlon <marmoneymar@icloud.com>
2019-09-04 17:50:27 -0400

    Initial Commit with Xcode proj instead

---
+++ HungryDevelopers - Command Line/HungryDevelopers - Command Line/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HungryDevelopers - Command Line
+//
+//  Created by Marlon Raskin on 9/4/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

2d25677a9ec46e4655003c8360b285bc1a1d627f
Jeffrey <SantanaJny@gmail.com>
2019-09-04 16:06:56 -0400

    Initial Commit

---
+++ SoupDay/SoupDay/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SoupDay
+//
+//  Created by Jeffrey Santana on 9/4/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

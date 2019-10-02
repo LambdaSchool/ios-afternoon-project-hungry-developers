d981f688c0d788e93171b4b3ee32000939532011
ladybeitel <ciarabeitel@gmail.com>
2019-10-02 13:19:24 -0400

    Initial commit

---
+++ HungryDevelopers/HungryDevelopers/Developer.swift
@@ -0,0 +1,26 @@
+//
+//  Developer.swift
+//  HungryDevelopers
+//
+//  Created by Ciara Beitel on 10/2/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+
+class Developer {
+    var leftSpoon
+    var rightSpoon
+    
+    func think() {
+        
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+        
+    }
+}
---
+++ HungryDevelopers/HungryDevelopers/Spoon.swift
@@ -0,0 +1,19 @@
+//
+//  Spoon.swift
+//  HungryDevelopers
+//
+//  Created by Ciara Beitel on 10/2/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+
+class Spoon {
+    func pickUp() {
+        
+    }
+    
+    func putDown() {
+        
+    }
+}

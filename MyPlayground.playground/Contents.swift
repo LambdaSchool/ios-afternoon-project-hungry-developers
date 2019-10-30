248d040677942237e02f54111b3bfd7389c545ea
Jerry Haaser <jerry.haaser@hotmail.com>
2019-10-30 17:46:01 -0500

    set up Spoon and Develeper classes

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+class Spoon {
+    let spoonLock = NSLock()
+
+    func pickUp() {
+        spoonLock.lock()
+    }
+    
+    func putDown() {
+        spoonLock.unlock()
+    }
+
+}
+
+extension NSLock {
+    func withLock(_ work: () -> Void) {
+        lock()
+        work()
+        unlock()
+    }
+}
+
+class Developer {
+    
+    let leftSpoon = Spoon()
+    let rightSpoon = Spoon()
+    
+    func run() {
+        func think() {
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
+        }
+        
+        func eat() {
+            usleep(5)
+        }
+    }
+}

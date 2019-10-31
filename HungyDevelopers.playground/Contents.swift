26f16065c4173f969bdad55cdb698c3a97540191
Wilma <kima_92@fastmail.com>
2019-10-31 06:26:11 -0400

    Added Developers class

--- HungyDevelopers.playground/Contents.swift
+++ HungyDevelopers.playground/Contents.swift
@@ -4,20 +4,47 @@ import Foundation
 
 
 class Spoon {
-    
-    let pickupLock = NSLock()
-    
-    DispatchQueue.concurrentPerform(iterations: 100) { _ in
-    
-    pickupLock.lock()
+    //
+    //    let pickupLock = NSLock()
+    //
+    //    DispatchQueue.concurrentPerform(iterations: 100) { _ in
+    //
+    //    pickupLock.lock()
     func pickup() {
-    
-    
+        
+        
     }
-    }
+    //    }
     
     
     func putDown() {
         
     }
+}
+
+
+class Developer {
+    
+    let leftSpoon: Spoon?
+    let rightSpoon: Spoon?
+    
+    //think() should pick up both spoons before returning.
+    func think() {
+        print("Picked up spoons")
+        
+        
+    }
+    
+    //eat() should pause for a random amount of time before putting both spoons down.
+    func eat() {
+        sleep(2)
+        
+        
+    }
+    
+    func run() {
+        think()
+        eat()
+    }
+    
 }

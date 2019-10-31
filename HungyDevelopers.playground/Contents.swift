ffd3d4b338e8c73d2d21e5ba30c86fa98d4b02bc
Wilma <kima_92@fastmail.com>
2019-10-31 06:16:35 -0400

    Added Spoon class

--- HungyDevelopers.playground/Contents.swift
+++ HungyDevelopers.playground/Contents.swift
@@ -3,3 +3,21 @@ import Foundation
 
 
 
+class Spoon {
+    
+    let pickupLock = NSLock()
+    
+    DispatchQueue.concurrentPerform(iterations: 100) { _ in
+    
+    pickupLock.lock()
+    func pickup() {
+    
+    
+    }
+    }
+    
+    
+    func putDown() {
+        
+    }
+}

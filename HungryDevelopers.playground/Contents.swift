b90ad72ac72d97ca1b86c46aa2e9fc8aeedec72b
Robert Keffury <bobby.keffury@me.com>
2019-11-05 10:13:06 -0800

    Finished Project

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,8 +1,10 @@
 import UIKit
+import Foundation
 
 class Spoon {
     
     let spoonLock = NSLock()
+    var index = Int()
     
     func pickUp() {
         spoonLock.lock()
@@ -16,40 +18,64 @@ class Spoon {
 
 class Developer {
     
-    var leftSpoon: Spoon? = nil
-    var rightSpoon: Spoon? = nil
+    var leftSpoon: Spoon?
+    var rightSpoon: Spoon?
+    
+    
+    
     
     func think() {
-        leftSpoon?.pickUp()
-        rightSpoon?.pickUp()
+        guard let leftSpoon = leftSpoon, let rightSpoon = rightSpoon else { return }
+        
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
+        } else {
+            rightSpoon.pickUp()
+            leftSpoon.pickUp()
+        }
     }
     
     func eat() {
-        sleep(5)
+        let startedEating = CACurrentMediaTime()
+        sleep(1)
         rightSpoon?.putDown()
         leftSpoon?.putDown()
+        let endedEating = CACurrentMediaTime()
+        let elapsedTime = endedEating - startedEating
+        print("Time spent eating: \(elapsedTime)")
     }
     
     func run() {
         think()
         eat()
+
     }
+    
+    
+    
     
 }
 
-let spoon1: Spoon 
-let spoon2: Spoon
-let spoon3: Spoon
-let spoon4: Spoon
-let spoon5: Spoon
+var spoon1 = Spoon()
+var spoon2 = Spoon()
+var spoon3 = Spoon()
+var spoon4 = Spoon()
+var spoon5 = Spoon()
+
+spoon1.index = 1
+spoon2.index = 2
+spoon3.index = 3
+spoon4.index = 4
+spoon5.index = 5
     
 let spoons: [Spoon] = [spoon1,spoon2,spoon3,spoon4,spoon5]
 
-let developer1: Developer
-let developer2: Developer
-let developer3: Developer
-let developer4: Developer
-let developer5: Developer
+let developer1 = Developer()
+let developer2 = Developer()
+let developer3 = Developer()
+let developer4 = Developer()
+let developer5 = Developer()
 
 let developers: [Developer] = [developer1,developer2,developer3,developer4,developer5]
     
@@ -67,4 +93,10 @@ developer4.rightSpoon = spoon5
 
 developer5.leftSpoon = spoon5
 developer5.rightSpoon = spoon1
+
+let sharedAccessQueue = DispatchQueue(label: "Shared Resource Queue")
+
+DispatchQueue.concurrentPerform(iterations: 5) {
+        developers[$0].run()
+}
 

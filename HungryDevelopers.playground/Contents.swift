4c56838b404b7f6a58f1415b27a69f92d67cf49c
Vici Shaweddy <vshaweddy@gmail.com>
2019-11-05 19:43:05 -0500

    fixed the logic for part four solution

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 class Spoon {
-    let index: Int
     private let spoonLock = NSLock()
+    let index: Int
     
     init(index: Int) {
         self.index = index
@@ -31,13 +31,30 @@ class Developer {
     func think() {
         print("Developer\(self.id) is thinking to eat.")
         
-        // a developer will always pick up their lower-numbered spoon first
+        // Caling first solution
+         self.firstSolution()
+//        if self.leftSpoon.index < self.rightSpoon.index {
+//        self.leftSpoon.pickUp()
+//        print("Developer\(self.id) picked up spoon \(leftSpoon.index).")
+//        self.rightSpoon.pickUp()
+//        print("Developer\(self.id) picked up spoon \(rightSpoon.index).")
+//        return
+//        }
+    }
+    
+    /* First Solution (Part Four - Solution) */
+    // a developer will always pick up their lower-numbered spoon first
+    func firstSolution() {
         if self.leftSpoon.index < self.rightSpoon.index {
             self.leftSpoon.pickUp()
             print("Developer\(self.id) picked up spoon \(leftSpoon.index).")
             self.rightSpoon.pickUp()
             print("Developer\(self.id) picked up spoon \(rightSpoon.index).")
-            return
+        } else {
+            self.rightSpoon.pickUp()
+            print("Developer\(self.id) picked up spoon \(rightSpoon.index).")
+            self.leftSpoon.pickUp()
+            print("Developer\(self.id) picked up spoon \(leftSpoon.index).")
         }
     }
     
@@ -52,7 +69,20 @@ class Developer {
         while true {
             self.think()
             self.eat()
-            print("Developer\(self.id) just finished.")
+        }
+    }
+}
+
+let group = DispatchGroup()
+let lockDeveloper = NSLock()
+
+/* Waiter (Arbitrator) Solution */
+class Waiter {
+    func wasAsked(by developer: Developer) {
+        DispatchQueue.global().async {
+            lockDeveloper.lock()
+            developer.run()
+            lockDeveloper.unlock()
         }
     }
 }
@@ -63,6 +93,8 @@ var spoon3 = Spoon(index: 3)
 var spoon4 = Spoon(index: 4)
 var spoon5 = Spoon(index: 5)
 
+var waiter = Waiter()
+
 var developer1 = Developer(id: 1, leftSpoon: spoon1, rightSpoon: spoon2)
 var developer2 = Developer(id: 2, leftSpoon: spoon2, rightSpoon: spoon3)
 var developer3 = Developer(id: 3, leftSpoon: spoon3, rightSpoon: spoon4)
@@ -71,5 +103,13 @@ var developer5 = Developer(id: 5, leftSpoon: spoon5, rightSpoon: spoon1)
 
 var developers = [developer1, developer2, developer3, developer4, developer5]
 
+// Only for first solution
 DispatchQueue.concurrentPerform(iterations: 5) { developers[$0].run() }
+
+// Waiter Solution
+//DispatchQueue.concurrentPerform(iterations: 5) { waiter.wasAsked(by: developers[$0]) }
+
+
+
+
 

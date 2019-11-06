35b274ed0ef70237d3af6369506137b156914399
Fabiola <saga.fabiola@gmail.com>
2019-11-06 10:55:47 -0600

    Made a container array to add each developer (created with an instance of Developer, and Spoon for index)

--- HungryDevelopers/HungryDevelopers/ViewController.swift
+++ HungryDevelopers/HungryDevelopers/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Properties
+    var devs: [Developer] = []
+    
+    // Developers
+    let developer1 = Developer(name: "Developer One", leftSpoon: Spoon(index: 5), rightSpoon: Spoon(index: 1))
+    let developer2 = Developer(name: "Developer Two", leftSpoon: Spoon(index: 1), rightSpoon: Spoon(index: 2))
+    let developer3 = Developer(name: "Developer Three", leftSpoon: Spoon(index: 2), rightSpoon: Spoon(index: 3))
+    let developer4 = Developer(name: "Developer Four", leftSpoon: Spoon(index: 3), rightSpoon: Spoon(index: 4))
+    let developer5 = Developer(name: "Developer Five", leftSpoon: Spoon(index: 4), rightSpoon: Spoon(index: 5))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 

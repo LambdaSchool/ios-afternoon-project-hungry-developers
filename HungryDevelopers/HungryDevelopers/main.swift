5c405892dd1af2c2196d03f0546ca6d5b862f963
Jon Bash <jonbash@protonmail.com>
2019-12-04 08:26:27 -0800

    sit up basics

--- HungryDevelopers/HungryDevelopers/main.swift
+++ HungryDevelopers/HungryDevelopers/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+class Spoon {
+    func pickUp() {
+        
+    }
+    
+    func putDown() {
+        
+    }
+}
 
+class Developer {
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    
+    var running: Bool = false
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+    
+    func think() {
+        
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+        while running {
+            think()
+            eat()
+        }
+    }
+}

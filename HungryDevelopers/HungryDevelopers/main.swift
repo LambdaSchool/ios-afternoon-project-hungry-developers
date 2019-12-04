1e7689e22fa9ee5997a1cd94a817d1041d5c210c
Jon Bash <jonbash@protonmail.com>
2019-12-04 08:14:30 -0800

    init xcode project

---
+++ HungryDevelopers/HungryDevelopers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HungryDevelopers
+//
+//  Created by Jon Bash on 2019-12-04.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

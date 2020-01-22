80760258b7bcea793f39e2fea47c2fee4b6f0d9c
Craig Swanson <cswanson02@me.com>
2020-01-21 18:36:12 -1000

    Starting with new ideas.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,8 +2,11 @@ import Foundation
 
 class Spoon {
     
+
+    private let lock = NSLock()
+    
     func pickUp() {
-        
+
     }
     
     func putDown() {
@@ -13,18 +16,32 @@ class Spoon {
 
 class Developer {
     
-    var leftSpoon: Spoon?
-    var rightSpoon: Spoon?
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+    
     
     func think() {
-        leftSpoon?.pickUp()
-        rightSpoon?.pickUp()
+//        first developer
+        // if left spoon is available, pick it up
+        // if not then wait
+        // when they have the left spoon, check if right one is available
+        // if it is, pick it up and then eat
+        // if not, wait until it's available.
+        
+        leftSpoon.pickUp()
+        rightSpoon.pickUp()
+            
     }
     
     func eat() {
         usleep(500)
-        leftSpoon?.putDown()
-        rightSpoon?.putDown()
+        rightSpoon.putDown()
+        leftSpoon.putDown()
     }
     
     func run() {
@@ -33,3 +50,19 @@ class Developer {
         run()
     }
 }
+
+var spoons: [Spoon] = []
+let s1 = Spoon()
+let s2 = Spoon()
+let s3 = Spoon()
+let s4 = Spoon()
+let s5 = Spoon()
+
+let d1 = Developer(leftSpoon: s1, rightSpoon: s5)
+let d2 = Developer(leftSpoon: s2, rightSpoon: s1)
+let d3 = Developer(leftSpoon: s3, rightSpoon: s2)
+let d4 = Developer(leftSpoon: s4, rightSpoon: s3)
+let d5 = Developer(leftSpoon: s5, rightSpoon: s4)
+let developers = [d1, d2, d3, d4, d5]
+
+DispatchQueue.concurrentPerform(iterations: 5) { developers[$0].run() }

ea7c5ab6adae6e1a45e4c540a7e9aa67030e16f3
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-02-16 18:28:53 -0800

    works except for 1 dev

--- HungryDevelopersAP/HungryDevelopersAP/ViewController.swift
+++ HungryDevelopersAP/HungryDevelopersAP/ViewController.swift
@@ -27,6 +27,10 @@ import UIKit
  Create 5 Spoons and 5 Developers giving each developer a left and right spoon. Note that developers will of course share spoons. Every developer's right spoon is the next developer's left spoon.
  Call run() on each developer in a different queue/thread. You can do this with the following code assuming you put your developers in an array:
 
+ DispatchQueue.concurrentPerform(iterations: 5) {
+ developers[$0].run()
+ }
+ 
  To recap the algorithm you're writing here, each developer will:
 
  think until the left spoon is available; when it is, pick it up;
@@ -40,44 +44,101 @@ import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
-//        var developers = [Developer]()
-//
-//        super.viewDidLoad()
-//        print("Test")
-//        DispatchQueue.concurrentPerform(iterations: 5) {
-//        developers[$0].run()
-//        }
+        super.viewDidLoad()
+        
+        let mac: Developer = Developer()
+        mac.name = "Mac"
+        let charlie: Developer = Developer()
+        charlie.name = "Charlie"
+        let dennis: Developer = Developer()
+        dennis.name = "Dennis"
+        let dee: Developer = Developer()
+        dee.name = "Dee"
+        let frank: Developer = Developer()
+        frank.name = "Frank"
+        
+        let spoon1 = Spoon()
+        spoon1.index = 1
+        let spoon2 = Spoon()
+        spoon2.index = 2
+        let spoon3 = Spoon()
+        spoon3.index = 3
+        let spoon4 = Spoon()
+        spoon4.index = 4
+        let spoon5 = Spoon()
+        spoon5.index = 5
+        
+        mac.rightSpoon = spoon1
+        charlie.leftSpoon = mac.rightSpoon
+        charlie.rightSpoon = spoon2
+        dennis.leftSpoon = charlie.rightSpoon
+        dennis.rightSpoon = spoon3
+        dee.leftSpoon = dennis.rightSpoon
+        dee.rightSpoon = spoon4
+        frank.leftSpoon = dee.rightSpoon
+        frank.rightSpoon = spoon5
+        mac.leftSpoon = frank.rightSpoon
+        
+        let developers: [Developer] = [mac, charlie, dennis, dee, frank]
+        
+        print("START")
+        // Last dev never runs for some reason (only if I take out think/eat from run())
+        DispatchQueue.concurrentPerform(iterations: 5) {
+            print("called run on index \($0)")
+            developers[$0].run()
+        }
     }
 }
 
 class Spoon {
     private var spoonLock = NSLock()
+    var index: Int = 0
+    /// Locks
     func pickUp() {
-        print("Pick Up")
+        print("Spoon \(index) Picked Up")
+        spoonLock.lock()
     }
     
+    /// Unlocks
     func putDown() {
-        print("Put Down")
+        print("Spoon \(index) Put Down")
+        spoonLock.unlock()
     }
 }
 
 class Developer {
-    let rightSpoon = Spoon()
-    let leftSpoon = Spoon()
+    var rightSpoon = Spoon()
+    var leftSpoon = Spoon()
+    var name = ""
     
     /// think() should pick up both spoons before returning.
     func think() {
-        print("Think")
+        print("\(name) is Thinking")
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+            print("\(name) picked up their left spoon (\(leftSpoon.index))")
+            rightSpoon.pickUp()
+            print("\(name) picked up their right spoon (\(rightSpoon.index))")
+        }
+        else {
+            rightSpoon.pickUp()
+            print("\(name) picked up their right spoon (\(rightSpoon.index))")
+            leftSpoon.pickUp()
+            print("\(name) picked up their left spoon (\(leftSpoon.index))")
+        }
     }
     
     /// eat() should pause for a random amount of time before putting both spoons down. (Hint: use usleep() to pause for a given number of microseconds).
     func eat() {
-        print("Eat")
+        print("\(name) is Eating")
+        usleep(690)
+        leftSpoon.putDown()
+        rightSpoon.putDown()
     }
     
     /// Developer.run() should call think() then eat() over and over again forever.
     func run() {
-        print("Run")
+        print("\(name) RUN")
         // runs forever
         while true {
             think()
@@ -85,3 +146,9 @@ class Developer {
         }
     }
 }
+
+/*
+ Add an index property to Spoon.
+ Give each spoon an index from 1 to 5.
+ Refactor think() function, so that a developer will always pick up their lower-numbered spoon first. (The order in which they put them down doesn't matter.)
+ */

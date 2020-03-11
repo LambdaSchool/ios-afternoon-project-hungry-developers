04fcffe442fd5e6c57165e016b449f8312a9ff68
Nick <ptnguyen1901@gmail.com>
2020-03-11 17:49:30 -0400

    Moved files to Xcode project

---
+++ HungryDeveloper/HungryDeveloper/Model/Developer.swift
@@ -0,0 +1,41 @@
+//
+//  Developer.swift
+//  HungryDeveloper
+//
+//  Created by Nick Nguyen on 3/11/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import Foundation
+class Developer {
+    
+    var leftSpoon : Spoon
+    var rightSpoon: Spoon
+    
+    init(leftSpoon: Spoon,rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+    
+    
+    
+    
+    
+    func think() {
+        
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+        
+    }
+    
+    
+    
+    
+    
+    
+}

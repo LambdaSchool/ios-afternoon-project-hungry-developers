65f2dd1ea321c02130283efc305ea9ae45216bef
Chris <chris.gonzales.82@gmail.com>
2020-03-11 12:28:27 -0700

    Created Developer class, create left and right spoon properties, create think(), eat(), and run() methods.

--- HumgryDeveloperes.playground/Contents.swift
+++ HumgryDeveloperes.playground/Contents.swift
@@ -3,13 +3,35 @@ import UIKit
 var str = "Hello, playground"
 
 
-class Spoon{
+class Spoon {
+    
+    private var lock = NSLock()
     
     func pickUp(){
-        
+        lock.lock()
     }
     
     func putDown(){
-        
+        lock.unlock()
     }
+}
+
+class Developer {
+    var leftSpoon = Spoon()
+    var rightSpoon = Spoon()
+    
+    func think() {
+        leftSpoon.pickUp()
+        rightSpoon.pickUp()
+    }
+    
+    func eat() {
+        usleep(UInt32.random(in: 1...3))
+    }
+    
+    func run() {
+        think()
+        eat()
+    }
+    
 }

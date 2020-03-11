00bacb4ef18fb382502f9e1f6c860b4c78b491bd
Enrique Gongora <EGTech24@gmail.com>
2020-03-11 13:57:46 -0700

    Most of the logic implemented. Few steps left

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -22,15 +22,39 @@ class Developer {
     var rightSpoon: Spoon?
     
     func think() {
-        
+        guard let leftSpoon = leftSpoon, let rightSpoon = rightSpoon else { return }
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
+        } else {
+            rightSpoon.pickUp()
+            leftSpoon.pickUp()
+        }
     }
     
     func eat() {
-        
+        print("Begin eating")
+        usleep(UInt32.random(in: 1000...10_000_000))
+        rightSpoon?.putDown()
+        leftSpoon?.putDown()
+        print("Stop eating")
     }
     
     func run() {
-        
+        think()
+        eat()
     }
 }
 
+// Create 5 Spoons and 5 Developers
+var spoon1 = Spoon()
+var spoon2 = Spoon()
+var spoon3 = Spoon()
+var spoon4 = Spoon()
+var spoon5 = Spoon()
+
+var developer1 = Developer()
+var developer2 = Developer()
+var developer3 = Developer()
+var developer4 = Developer()
+var developer5 = Developer()

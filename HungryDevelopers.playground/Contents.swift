708b87784200331fdaef26eb01b93cff1f65446d
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-03-17 21:05:19 -0400

    Spoon class created and implemented pickUp() and putDown()

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
-
-var str = "Hello, playground"
+// 1. Create a class called Spoon. It should have two methods, pickUp() and putDown().
+class Spoon {
+// 2. If pickUp() is called while the spoon is in use by another developer, pickUp() should wait until the other developer calls putDown(). You can implement this with a private lock property.
+    let spoonLock = NSLock()
+    var index = Int()
+    
+    func pickUp() {
+        spoonLock.lock()
+    }
+    
+    func putDown() {
+        spoonLock.unlock()
+    }
+}

8ff1bed89a11f17f84878ecbce9de008b57e9702
Gerardo <gahernandez02@gmail.com>
2020-03-17 22:27:32 -0700

    Modified classes for simple lock

--- HungryDevelopers/HungryDevelopers/Spoon.swift
+++ HungryDevelopers/HungryDevelopers/Spoon.swift
@@ -7,99 +7,83 @@
 //
 
 import Foundation
-import UIKit
 
- var hasSpoon: Bool = false
- var sharedResource = hasSpoon
- let group = DispatchGroup()
- let myQueue = DispatchQueue(label: "Shared Access Queue")
-let spoonCount = 5
-let developerCount = 5
-
+var hasSpoon: Bool = false
+var sharedResource = hasSpoon
+let group = DispatchGroup()
+let myQueue = DispatchQueue(label: "Shared Access Queue")
+//let spoonCount = 5
+//let developerCount = 5
+//var index = 0
 
 class Spoon {
+    var index = 0
     
-    let developer = Developer()
-//    var numberOfIterations = 0
-     
-    
-    func pickUp() {
-        
-        for _ in 0..<spoonCount / developerCount{
-            
-            group.enter()
-            
-            myQueue.async {
-                while sharedResource == true {
-//                    self.numberOfIterations += 1
-
-                    self.developer.eat()
-                    
-                    if sharedResource == false {
-                        self.putDown()
-                        break
-                    }
-                }
-               group.leave()
-            }
-        }
+    init(index: Int) {
+        self.index = index
     }
     
+    func pickUp() {
+        group.enter()
+    }
     
     func putDown() {
-        
-        for _ in 0..<developerCount / spoonCount {
-            
-            group.enter()
-            
-        myQueue.async {
-            while sharedResource == false {
-            self.developer.think()
-            
-                
-                if sharedResource == true {
-                    self.pickUp()
-                    break
-                }
-            }
-            group.leave()
-            }
-        }
+        group.leave()
     }
 }
 
 class Developer {
-   
-    let leftSpoon: Bool = false
-    let rightSpoon: Bool = false
-    let spoon = Spoon()
+    let name: String
+    let leftSpoon: Spoon
+    let rightSpoon: Spoon
+    
+    init(name: String, leftSppon: Spoon, rightSpoon: Spoon) {
+        self.name = name
+        self.leftSpoon = leftSppon
+        self.rightSpoon = rightSpoon
+    }
     
     func think() {
-        
-        for _ in 0..<spoonCount / developerCount {
-            group.enter()
-            spoon.pickUp()
-            group.leave()
-            
-            while leftSpoon.self && rightSpoon.self != true {
-                group.enter()
-                sharedResource = false
-                spoon.putDown()
-                
-                if sharedResource == true {
-                    spoon.pickUp()
-                    break
-                }
-            }
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
+        } else {
+            rightSpoon.pickUp()
+            leftSpoon.pickUp()
+            return
         }
         
     }
     
     func eat() {
-        sleep(1)
+        usleep(32)
+        leftSpoon.putDown()
+        rightSpoon.putDown()
+        print("Done eating")
+        return
     }
     
     func run() {
+        while sharedResource == true {
+            think()
+            eat()
+        }
         
     }
 }
+
+var spoon1 = Spoon(index: 1),
+spoon2 = Spoon(index: 2),
+spoon3 = Spoon(index: 3),
+spoon4 = Spoon(index: 4),
+spoon5 = Spoon(index: 5)
+
+let developer1 = Developer(name: "Bruce Wayne", leftSppon: spoon1, rightSpoon: spoon2),
+developer2 = Developer(name: "Kobe Bryant", leftSppon: spoon2, rightSpoon: spoon3),
+developer3 = Developer(name: "Johnny Hicks", leftSppon: spoon3, rightSpoon: spoon4),
+developer4 = Developer(name: "Steven Berard", leftSppon: spoon4, rightSpoon: spoon5),
+developer5 = Developer(name: "Luqmaan Khan", leftSppon: spoon5, rightSpoon: spoon1)
+
+
+var developersArray: [Developer] = []
+developersArray.append(developer1,developer2,developer3,developer4,developer5)

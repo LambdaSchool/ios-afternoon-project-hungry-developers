5a7858bb6abc5570f3ae8b0447ed12a5ba4d4e2e
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-03-17 21:32:04 -0400

    MVP

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -36,6 +36,8 @@ class Developer {
             rs.pickUp()
             ls.pickUp()
         }
+        print("Done thinking")
+        return
     }
     
     func eat() {
@@ -44,6 +46,40 @@ class Developer {
         usleep(10000) // (Hint: use usleep() to pause for a given number of microseconds).
         leftSpoon?.putDown()
         rightSpoon?.putDown()
-        
+        print("Done eating")
+        return
     }
 }
+
+//7.Create 5 Spoons and 5 Developers giving each developer a left and right spoon.
+var spoon1 = Spoon(), spoon2 = Spoon(), spoon3 = Spoon(), spoon4 = Spoon(), spoon5 = Spoon()
+
+spoon1.index = 1
+spoon2.index = 2
+spoon3.index = 3
+spoon4.index = 4
+spoon5.index = 5
+
+let developer1 = Developer(), developer2 = Developer(), developer3 = Developer(), developer4 = Developer(), developer5 = Developer()
+
+//8. Call run() on each developer in a different queue/thread.
+var hungryDevelopers: [Developer] = []
+hungryDevelopers.append(contentsOf: [developer1,developer2,developer3,developer4,developer5])
+
+developer1.leftSpoon = spoon1
+developer1.rightSpoon = spoon2
+
+developer2.leftSpoon = spoon2
+developer2.rightSpoon = spoon3
+
+developer3.leftSpoon = spoon3
+developer3.rightSpoon = spoon4
+
+developer4.leftSpoon = spoon4
+developer4.rightSpoon = spoon5
+
+developer5.leftSpoon = spoon5
+developer5.rightSpoon = spoon1
+
+DispatchQueue.concurrentPerform(iterations: 5) { hungryDevelopers[$0].run() }
+

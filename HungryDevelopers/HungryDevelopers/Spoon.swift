f973057a8e4f334126cffe353d6c80a68791cc01
Gerardo <gahernandez02@gmail.com>
2020-03-17 20:11:20 -0700

    Spoon Class

--- HungryDevelopers/HungryDevelopers/Spoon.swift
+++ HungryDevelopers/HungryDevelopers/Spoon.swift
@@ -9,12 +9,72 @@
 import Foundation
 import UIKit
 
-class Spoon {
+
+class Spoon: Developer {
+    
+    static var hasSpoon: Bool = false
+    var sharedResource = hasSpoon
+    let group = DispatchGroup()
+    let myQueue = DispatchQueue(label: "Shared Access Queue")
+    let spoonCount = 5
+    let developer = Developer()
+//    var numberOfIterations = 0
+    
     func pickUp() {
+        
+        for _ in 0..<spoonCount / developer.count {
+            
+            self.group.enter()
+            
+            myQueue.async {
+                while self.sharedResource == true {
+//                    self.numberOfIterations += 1
+                    self.developer.eat()
+                    
+                    if self.sharedResource == false {
+                        self.putDown()
+                    }
+                }
+               self.group.leave()
+            }
+        }
+    }
+    
+    
+    func putDown() {
+        
+        for _ in 0..<developer.count / spoonCount {
+            
+            self.group.enter()
+            
+        myQueue.async {
+            while self.sharedResource == false {
+            self.developer.think()
+                
+                if self.sharedResource == true {
+                    self.pickUp()
+                }
+            }
+            self.group.leave()
+            }
+        }
+    }
+}
+
+class Developer {
+    let count = 5
+    let leftSpoon: Bool = false
+    let rightSpoon: Bool = false
+    
+    func think() {
         
     }
     
-    func putDown() {
+    func eat() {
+        sleep(1)
+    }
+    
+    func run() {
         
     }
 }

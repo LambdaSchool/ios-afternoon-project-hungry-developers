88bfa35048099e19e9a085b533fe83756685891a
Jessie <jessie.griffin@icloud.com>
2020-03-17 22:06:15 -0400

    Seems to be working except Developer#3 never gets to eat...

--- HungryDevelopersConcurrency.playground/Contents.swift
+++ HungryDevelopersConcurrency.playground/Contents.swift
@@ -18,18 +18,23 @@ class Developer {
     let times = 0
     let leftSpoon: Spoon
     let rightSpoon: Spoon
-    let seconds: UInt32 = UInt32.random(in: 0 ... 5)
+//    let seconds: UInt32 = UInt32.random(in: 0 ... 5)
+    let seconds: UInt32 = 100000
+    let index: Int = Int.random(in: 1...5)
     
     init() {
         leftSpoon = Spoon()
         rightSpoon = Spoon()
     }
+    
     private func think() {
+        print("Developer #\(index) is thinking")
         leftSpoon.pickUp()
         rightSpoon.pickUp()
     }
     
     private func eat() {
+        print("Developer #\(index) is eating.")
         usleep(seconds)
         rightSpoon.putDown()
         leftSpoon.putDown()

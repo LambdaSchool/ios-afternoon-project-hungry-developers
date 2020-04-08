ee749f7ebad20c9b937a83a9adef8b8e2307fa70
Brad <braddiroff@icloud.com>
2020-04-08 17:23:54 -0400

    Process works, fixing when it jams

--- HungryDev.playground/Contents.swift
+++ HungryDev.playground/Contents.swift
@@ -1,63 +1,64 @@
 import Cocoa
 
+
 class Spoon {
-    var lock = NSLock()
-    var locked = false
+    private let lock = NSLock()
+ //   var locked = false
     func pickUp() {
+        print("attemping pickup")
         lock.lock()
-        locked = true
+        print("successful pickup")
+//        locked = true
     }
     
     func putDown() {
+        print("attemping putdown")
         lock.unlock()
-        locked = false
+        print("successful putdown")
+//        locked = false
     }
 }
 
 class Developer {
-    var leftSpoon: Spoon {
-        didSet {
-            print("someone got the left")
-        }
-    }
-    var rightSpoon: Spoon {
-           didSet {
-              print("someone got the left")
-           }
-       }
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    var id: String
+    var number: UInt32 = 0
     
-    var holdingLeft = false
-    var holdingRight = false
+//    var holdingLeft = false
+//    var holdingRight = false
     
-    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+    init(leftSpoon: Spoon, rightSpoon: Spoon, identifier: String) {
         self.leftSpoon = leftSpoon
         self.rightSpoon = rightSpoon
+        self.id = identifier
     }
     
     func think() {
+        print("attempting left pickup  \(id)")
         leftSpoon.pickUp()
+        print("leftspoon pickup  \(id)")
+        print("attempting right pickup  \(id)")
         rightSpoon.pickUp()
-        print("think pickUp")
-        /*
-        repeat {
-            print("think pickUp")
-        } while leftSpoon.locked && rightSpoon.locked
-        */
+        print("rightspoon pickUp  \(id)")
         
         eat()
     }
     
     func eat() {
-        sleep(3)
-       // usleep(5)
-        print("eating for 5 seconds")
+        print("sleeping for 5 miliseconds  \(id)")
+        usleep(5)
+        print("finished eating  \(id)")
         rightSpoon.putDown()
+        print("rightspoon putDown  \(id)")
         leftSpoon.putDown()
+        print("leftspoon putDown  \(id)")
         think()
     }
     
     func run() {
         print("it's running now")
+        usleep(number)
         think()
         eat()
     }
@@ -67,16 +68,20 @@ class Developer {
 var nwSpoon = Spoon()
 var neSpoon = Spoon()
 var wSpoon = Spoon()
-var eSpoon = Spoon()
+//var eSpoon = Spoon()
 var sSpoon = Spoon()
 
-var nDev = Developer(leftSpoon: neSpoon, rightSpoon: nwSpoon)
-var wDev = Developer(leftSpoon: nwSpoon, rightSpoon: wSpoon)
-var swDev = Developer(leftSpoon: wSpoon, rightSpoon: sSpoon)
-var seDev = Developer(leftSpoon: sSpoon, rightSpoon: eSpoon)
-var eDev = Developer(leftSpoon: eSpoon, rightSpoon: neSpoon)
+var nDev = Developer(leftSpoon: neSpoon, rightSpoon: nwSpoon, identifier: "NORTH")
+var wDev = Developer(leftSpoon: nwSpoon, rightSpoon: wSpoon, identifier: "WEST")
+var swDev = Developer(leftSpoon: wSpoon, rightSpoon: sSpoon, identifier: "SW")
+var seDev = Developer(leftSpoon: sSpoon, rightSpoon: neSpoon, identifier: "SE")
+//var eDev = Developer(leftSpoon: eSpoon, rightSpoon: neSpoon, identifier: "EAST")
 
-var developers = [nDev, wDev, swDev, seDev, eDev]
+var developers = [nDev, wDev, swDev, seDev] //, eDev]
+
+for i in 0..<developers.count {
+    developers[i].number = UInt32(i)
+}
 
 DispatchQueue.concurrentPerform(iterations: 5) {
 developers[$0].run()

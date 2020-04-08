9e99dc9babfb12d1f3766ae575fed56b9a62c2cf
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-08 22:29:44 +0100

    Developer and Spoon Instances Created

--- Hungry Developers.playground/Contents.swift
+++ Hungry Developers.playground/Contents.swift
@@ -49,3 +49,21 @@ class Developer {
         eat()
     }
 }
+
+let spoon1 = Spoon()
+let spoon2 = Spoon()
+let spoon3 = Spoon()
+let spoon4 = Spoon()
+let spoon5 = Spoon()
+
+let developer1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2)
+let developer2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3)
+let developer3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
+let developer4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5)
+let developer5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1)
+
+let developers = [developer1, developer2, developer3, developer4, developer5]
+
+DispatchQueue.concurrentPerform(iterations: 5) {
+    developers[$0].run()
+}

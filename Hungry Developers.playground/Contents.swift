f0dad987b54df23c3fbd3c05997048ab45462f79
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-08 21:50:19 +0100

    Spoon Class Created

--- Hungry Developers.playground/Contents.swift
+++ Hungry Developers.playground/Contents.swift
@@ -3,4 +3,16 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+class Spoon {
+    private let spoonLock = NSLock()
+
+    func pickUp() {
+        spoonLock.lock()
+    }
+    
+    func putDown() {
+        spoonLock.unlock()
+    }
+}
+
 

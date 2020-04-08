ec8644bda29fb0fb9aa6426acdd1deab3e6ce47f
Tobi Tselios <tobi.tselios@gmail.com>
2020-04-08 21:50:45 +0100

    Developer Class Created

--- Hungry Developers.playground/Contents.swift
+++ Hungry Developers.playground/Contents.swift
@@ -15,4 +15,37 @@ class Spoon {
     }
 }
 
-
+class Developer {
+    let leftSpoon: Spoon
+    let rightSpoon: Spoon
+    
+    let pairOfSpoons = 2
+    let spoonsGroup = DispatchGroup()
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+    
+    func think() {
+        for _ in 0..<pairOfSpoons {
+            spoonsGroup.enter()
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
+            spoonsGroup.leave()
+        }
+        
+        spoonsGroup.wait()
+    }
+    
+    func eat() {
+        usleep(100_000)
+        leftSpoon.putDown()
+        rightSpoon.putDown()
+    }
+    
+    func run() {
+        think()
+        eat()
+    }
+}

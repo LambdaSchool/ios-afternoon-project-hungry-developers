626b1c89f2ae28e7828596125c8aa2e8c9c377ce
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-06 14:18:05 -0700

    Added some changed to fix the strategy for 5 developers

--- HungryDevelopersProject/HungryDevelopersProject/main.swift
+++ HungryDevelopersProject/HungryDevelopersProject/main.swift
@@ -29,17 +29,18 @@ class Spoon {
 
 class Developer{
     var id: String
-    let leftSpoon: Spoon
-    let rightSpoon: Spoon
+    var leftSpoon: Spoon?
+    var rightSpoon: Spoon?
     var numberOfSpoon = 0
     
-    init(leftSpoon: Spoon, rightSpoon: Spoon, id: String) {
-        self.leftSpoon = leftSpoon
-        self.rightSpoon = rightSpoon
+    init(id: String) {
         self.id = id
     }
     
     func eat() {
+        guard let leftSpoon = leftSpoon,
+            let rightSpoon = rightSpoon else { return }
+        
         usleep(useconds_t(Int.random(in: 1...1_000_000)))
         leftSpoon.putDown()
         rightSpoon.putDown()
@@ -47,6 +48,8 @@ class Developer{
     }
     
     func think() {
+        guard let leftSpoon = leftSpoon,
+            let rightSpoon = rightSpoon else { return }
         if leftSpoon.index < rightSpoon.index {
             leftSpoon.pickUp()
             rightSpoon.pickUp()
@@ -71,6 +74,25 @@ let spoon3 = Spoon(3)
 let spoon4 = Spoon(4)
 let spoon5 = Spoon(5)
 
+let developer1 = Developer(id: "Developer 1")
+developer1.leftSpoon = spoon1
+developer1.rightSpoon = spoon2
+
+let developer2 = Developer(id: "Developer 2")
+developer2.leftSpoon = spoon2
+developer2.rightSpoon = spoon3
+
+let developer3 = Developer(id: "Developer 3")
+developer3.leftSpoon = spoon3
+developer3.rightSpoon = spoon4
+
+let developer4 = Developer(id: "Developer 4")
+developer4.leftSpoon = spoon4
+developer4.rightSpoon = spoon5
+
+let developer5 = Developer(id: "Developer 5")
+developer5.leftSpoon = spoon5
+developer5.rightSpoon = spoon1
 
 
 

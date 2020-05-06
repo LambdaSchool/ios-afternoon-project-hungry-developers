e40a2ab4898e96d11330853bb8fbb0accaa9bed8
Shawn James <shawn.james@me.com>
2020-05-06 16:36:45 -0400

    Create spoon class and move over the steps to part 1

--- HungryDevs.playground/Contents.swift
+++ HungryDevs.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+class Spoon {
+    
+    // when spoon should be pickup up
+    func pickUp() {
+        
+    }
+    
+    // we spoon should be put down
+    func putDown() {
+        
+    }
+    
+}
+
+//Create a class called Spoon. It should have two methods, pickUp() and putDown().
+//If pickUp() is called while the spoon is in use by another developer, pickUp() should wait until the other developer calls putDown(). You can implement this with a private lock property.
+//Create a class called Developer. Each Developer should have a leftSpoon property and a rightSpoon property. It should also have think(), eat(), and run() methods.
+//Developer.run() should call think() then eat() over and over again forever.
+//think() should pick up both spoons before returning.
+//eat() should pause for a random amount of time before putting both spoons down. (Hint: use usleep() to pause for a given number of microseconds).
+//Create 5 Spoons and 5 Developers giving each developer a left and right spoon. Note that developers will of course share spoons. Every developer's right spoon is the next developer's left spoon.
+//Call run() on each developer in a different queue/thread. You can do this with the following code assuming you put your developers in an array:
+//DispatchQueue.concurrentPerform(iterations: 5) {
+//developers[$0].run()
+//}
+//To recap the algorithm you're writing here, each developer will:
+//
+//think until the left spoon is available; when it is, pick it up;
+//think until the right spoon is available; when it is, pick it up;
+//when both spoons are held, eat for a fixed amount of time;
+//then, put the right spoon down;
+//then, put the left spoon down;
+//repeat from the beginning.

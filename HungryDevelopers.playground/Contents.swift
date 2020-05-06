21beb486956038b847527969992090395a84e739
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-06 16:16:17 -0500

    Finished

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -3,13 +3,17 @@ import Foundation
 //MARK: - Properties and Classes
 class Spoon {
     var index: Int
+    private(set) var isLocked: Bool = false
     private var lock = NSLock()
+    
     func pickUp() {
         lock.lock()
+        isLocked = true
     }
     
     func putDown() {
         lock.unlock()
+        isLocked = false
     }
     
     init(index: Int) {
@@ -32,6 +36,12 @@ class Developer {
     }
     
     func eat() {
+        if leftSpoon.isLocked {
+            rightSpoon.pickUp()
+        } else {
+            leftSpoon.pickUp()
+        }
+        
         usleep(5)
         print("Eating")
     }
@@ -55,11 +65,11 @@ var developers: [Developer] = [Developer(leftSpoon: spoons[0], rightSpoon: spoon
                                Developer(leftSpoon: spoons[1], rightSpoon: spoons[2]),
                                Developer(leftSpoon: spoons[2], rightSpoon: spoons[3]),
                                Developer(leftSpoon: spoons[3], rightSpoon: spoons[4]),
-                               Developer(leftSpoon: spoons[4], rightSpoon: spoons[1])]
-
-
+                               Developer(leftSpoon: spoons[4], rightSpoon: spoons[0])]
 
 
 DispatchQueue.concurrentPerform(iterations: 5) {
     developers[$0].run()
 }
+
+print("Finished")

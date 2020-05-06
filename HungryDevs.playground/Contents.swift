5e72f458c43d1c17314e157682414d4e3b3a820b
Harmony Radley <harmonyradley99@gmail.com>
2020-05-06 17:27:01 -0400

    Created class for Developer and added 3 methods and two properties to it

--- HungryDevs.playground/Contents.swift
+++ HungryDevs.playground/Contents.swift
@@ -2,14 +2,36 @@ import UIKit
 
 class Spoon {
     
+    private var lock = NSLock()
+    
     func pickUp() {
         
     }
     
+    
     func putDown() {
         
     }
-    
-    
-    
 }
+
+
+class Developer {
+    
+    var leftSpoon = Int()
+    var rightSpoon = Int()
+    
+    func think() {
+      
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+        
+    }
+}
+
+
+

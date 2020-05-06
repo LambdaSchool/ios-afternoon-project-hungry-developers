792680932e017c32fca43f554922d68ea8197ba6
Harmony Radley <harmonyradley99@gmail.com>
2020-05-06 17:46:47 -0400

    Completed steps 5-8 of part 1

--- HungryDevs.playground/Contents.swift
+++ HungryDevs.playground/Contents.swift
@@ -27,16 +27,39 @@ class Developer {
     }
         
     func think() {
-      
+        leftSpoon.pickUp()
+        rightSpoon.pickUp()
     }
     
     func eat() {
-        
+        usleep(100)
     }
     
     func run() {
         
     }
+}
+
+// 5 Spoons
+let spoon1 = Spoon()
+let spoon2 = Spoon()
+let spoon3 = Spoon()
+let spoon4 = Spoon()
+let spoon5 = Spoon()
+
+// 5 Developers
+let developer1 = Developer(name: "Carl", leftSpoon: spoon1, rightSpoon: spoon2)
+let developer2 = Developer(name: "Mom", leftSpoon: spoon2, rightSpoon: spoon3)
+let developer3 = Developer(name: "Dad", leftSpoon: spoon3, rightSpoon: spoon4)
+let developer4 = Developer(name: "Sister", leftSpoon: spoon4, rightSpoon: spoon5)
+let developer5 = Developer(name: "My Dog", leftSpoon: spoon5, rightSpoon: spoon1)
+
+let developers = [developer1, developer2, developer3, developer4, developer5]
+
+
+// Calling each developer in a different thread/queue 
+DispatchQueue.concurrentPerform(iterations: 5) {
+developers[$0].run()
 }
 
 

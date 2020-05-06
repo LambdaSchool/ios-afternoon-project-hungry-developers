a061f594e6a01353a2acbd08fb33311c0d2e4747
Chris Dobek <dobek.christopher@gmail.com>
2020-05-06 15:44:27 -0400

    Spoons and Developers Created

--- Hungry Developers.playground/Contents.swift
+++ Hungry Developers.playground/Contents.swift
@@ -43,3 +43,17 @@ class Developer {
         eat()
     }
 }
+
+let spoon1 = Spoon()
+let spoon2 = Spoon()
+let spoon3 = Spoon()
+let spoon4 = Spoon()
+let spoon5 = Spoon()
+
+let developer1 = Developer(name: "Chris", leftSpoon: spoon1, rightSpoon: spoon2)
+let developer2 = Developer(name: "Tommy", leftSpoon: spoon2, rightSpoon: spoon3)
+let developer3 = Developer(name: "Ariana", leftSpoon: spoon3, rightSpoon: spoon4)
+let developer4 = Developer(name: "Jessica", leftSpoon: spoon4, rightSpoon: spoon5)
+let developer5 = Developer(name: "David", leftSpoon: spoon5, rightSpoon: spoon1)
+
+

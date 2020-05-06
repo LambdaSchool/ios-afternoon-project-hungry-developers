698091762e26ef593e7397a45cc65e20a9918573
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-06 12:46:07 -0700

    Added run, eat, think

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -2,15 +2,28 @@ import Cocoa
 
 class Spoon {
     static func pickUp() {
-        
+        print("Picked up spoon")
     }
     
     static func putDown() {
-        
+        print("Put down spoon")
     }
 }
 
 class Developer {
     let leftSpoon = ""
     let rightSpoon = ""
+    
+    static func eat() {
+        print("eating")
+    }
+    
+    static func think() {
+        print("thinking")
+    }
+    
+    static func run() {
+        print("running")
+    }
+    
 }

d0fbb7a1b38949a629262daa31d0b82c85089001
Shawn James <shawn.james@me.com>
2020-05-06 17:32:16 -0400

    -Adjust various comments -Create an array holding all developers -Call run() on all Developers

--- HungryDevs.playground/Contents.swift
+++ HungryDevs.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 import PlaygroundSupport
 
+// MARK: - Creating The Classes
+
 class Spoon {
     
     // MARK: - Properties
@@ -64,7 +66,7 @@ class Developer {
     
 }
 
-// MARK: - Spoons
+// MARK: - Create Spoons
 
 let spoon0 = Spoon()
 let spoon1 = Spoon()
@@ -72,7 +74,7 @@ let spoon2 = Spoon()
 let spoon3 = Spoon()
 let spoon4 = Spoon()
 
-// MARK: - Developers
+// MARK: - Create Developers
 
 let developer0 = Developer(leftSpoon: spoon0, rightSpoon: spoon1)
 let developer1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2)
@@ -80,11 +82,14 @@ let developer2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3)
 let developer3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
 let developer4 = Developer(leftSpoon: spoon4, rightSpoon: spoon0)
 
-//Create 5 Spoons and 5 Developers giving each developer a left and right spoon. Note that developers will of course share spoons. Every developer's right spoon is the next developer's left spoon.
-//Call run() on each developer in a different queue/thread. You can do this with the following code assuming you put your developers in an array:
-//DispatchQueue.concurrentPerform(iterations: 5) {
-//developers[$0].run()
-//}
+let allDevelopers = [developer0, developer1, developer2, developer3, developer4]
+
+// MARK: - Running The Program
+
+DispatchQueue.concurrentPerform(iterations: 5) {
+    allDevelopers[$0].run()
+}
+
 //To recap the algorithm you're writing here, each developer will:
 //
 //think until the left spoon is available; when it is, pick it up;

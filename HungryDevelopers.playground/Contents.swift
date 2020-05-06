29a5ce247260c114ba4680a60d0167338f83f150
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-06 14:30:21 -0500

    Added Classes and assigned Developers Spoons

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,4 +1,48 @@
 import Foundation
 
+//MARK: - Properties and Classes
+
+class Spoon {
+    private var lock = NSLock()
+    func pickUp() {
+        lock.lock()
+    }
+    
+    func putDown() {
+        lock.unlock()
+    }
+}
 
 
+class Developer {
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    
+    func think() {
+        leftSpoon.pickUp()
+        rightSpoon.pickUp()
+    }
+    
+    func eat() {
+        usleep(5)
+    }
+    
+    func run() {
+        think()
+        eat()
+    }
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+}
+
+//Creating Spoons and assigning them to developers
+var spoons: [Spoon] = [Spoon(), Spoon(), Spoon(), Spoon(), Spoon()]
+
+var developers: [Developer] = [Developer(leftSpoon: spoons[0], rightSpoon: spoons[1]),
+                               Developer(leftSpoon: spoons[1], rightSpoon: spoons[2]),
+                               Developer(leftSpoon: spoons[2], rightSpoon: spoons[3]),
+                               Developer(leftSpoon: spoons[3], rightSpoon: spoons[4]),
+                               Developer(leftSpoon: spoons[4], rightSpoon: spoons[1])]

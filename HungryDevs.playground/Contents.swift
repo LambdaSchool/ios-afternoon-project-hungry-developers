a15e6225634b474b6812d099dddc4f8ff0724e98
Shawn James <shawn.james@me.com>
2020-05-06 17:27:09 -0400

    Create spoons and Developers

--- HungryDevs.playground/Contents.swift
+++ HungryDevs.playground/Contents.swift
@@ -64,6 +64,22 @@ class Developer {
     
 }
 
+// MARK: - Spoons
+
+let spoon0 = Spoon()
+let spoon1 = Spoon()
+let spoon2 = Spoon()
+let spoon3 = Spoon()
+let spoon4 = Spoon()
+
+// MARK: - Developers
+
+let developer0 = Developer(leftSpoon: spoon0, rightSpoon: spoon1)
+let developer1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2)
+let developer2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3)
+let developer3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
+let developer4 = Developer(leftSpoon: spoon4, rightSpoon: spoon0)
+
 //Create 5 Spoons and 5 Developers giving each developer a left and right spoon. Note that developers will of course share spoons. Every developer's right spoon is the next developer's left spoon.
 //Call run() on each developer in a different queue/thread. You can do this with the following code assuming you put your developers in an array:
 //DispatchQueue.concurrentPerform(iterations: 5) {

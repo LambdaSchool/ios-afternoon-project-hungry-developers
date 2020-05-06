7663dcf9cb557ba3bdbcc06e6e31d6eebca2da92
Hunter Oppel <hunter.oppel@gmail.com>
2020-05-06 15:34:11 -0400

    Added spoon and developer class skeletons.

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+class Spoon {
+    private var spoonLock = NSLock()
+    
+    func pickUp() {
+        spoonLock.lock()
+    }
+    
+    func putDown() {
+        spoonLock.unlock()
+    }
+}
+
+class Developer {
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    
+    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+    
+    func think() {
+        
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+        
+    }
+}

df3921d65ae0a2746c3f4765bc7919fae856b10e
Matt Martindale <martindalematt18@yahoo.com>
2020-05-11 21:41:18 -0600

    implemented pickUp and putDown methods

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -2,11 +2,13 @@ import UIKit
 
 class Spoon {
     func pickUp() {
-        
+        lock.lock()
+        putDown()
     }
     
     func putDown() {
         
+        lock.unlock()
     }
     
     private let lock = NSLock()

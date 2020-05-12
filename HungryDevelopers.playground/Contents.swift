d763fd07f3cac72f1cd553cf7ced95969efc667f
Matt Martindale <martindalematt18@yahoo.com>
2020-05-11 21:46:18 -0600

    started Developer class

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -13,3 +13,27 @@ class Spoon {
     
     private let lock = NSLock()
 }
+
+class Developer {
+    var leftSpoon: Bool
+    var rightSpoon: Bool
+    
+    func think() {
+        Spoon.pickUp(Spoon.init())
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+        think()
+        eat()
+        run()
+    }
+    
+    init(leftSpoon: Bool, rightSpoon: Bool) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+}

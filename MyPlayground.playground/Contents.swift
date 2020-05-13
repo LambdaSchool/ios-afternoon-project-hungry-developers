aa552c12f1adb824f8e00136761e11b455c865c4
Breena <breena_greek@live.com>
2020-05-12 22:38:27 -0400

    MVP

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,6 +4,12 @@ class Spoon {
     
     private let lock = NSLock()
     
+    let index: Int
+    
+    init(index: Int) {
+        self.index = index
+    }
+    
     func pickUp() {
         lock.lock()
     }
@@ -14,38 +20,53 @@ class Spoon {
 }
 
 class Developer {
+    var name: String
     let leftSpoon: Spoon
     let rightSpoon: Spoon
     
-    init(leftSpoon: Spoon, rightSpoon: Spoon) {
+    init(leftSpoon: Spoon, rightSpoon: Spoon, name: String) {
+        self.name = name
         self.leftSpoon = leftSpoon
         self.rightSpoon = rightSpoon
     }
     
     func think() {
-        
+        if leftSpoon.index < rightSpoon.index {
+            leftSpoon.pickUp()
+            rightSpoon.pickUp()
+        } else {
+            rightSpoon.pickUp()
+            leftSpoon.pickUp()
+        }
     }
     
     func eat() {
-        
+        print("\(name) started eating")
+        usleep(.random(in: 1...10))
+        leftSpoon.putDown()
+        rightSpoon.putDown()
+        print("\(name) stopped eating")
     }
     
     func run() {
-        
+        while true {
+        think()
+        eat()
+        }
     }
 }
 
-let spoon1 = Spoon()
-let spoon2 = Spoon()
-let spoon3 = Spoon()
-let spoon4 = Spoon()
-let spoon5 = Spoon()
+let spoon1 = Spoon(index: 1)
+let spoon2 = Spoon(index: 2)
+let spoon3 = Spoon(index: 3)
+let spoon4 = Spoon(index: 4)
+let spoon5 = Spoon(index: 5)
 
-let developer1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2)
-let developer2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3)
-let developer3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
-let developer4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5)
-let developer5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1)
+let developer1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2, name: "Breena")
+let developer2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3, name: "Jordan")
+let developer3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4, name: "Janaiya")
+let developer4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5, name: "River")
+let developer5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1, name: "PapaBear")
 
 let developers = [developer1, developer2, developer3, developer4, developer5]
 

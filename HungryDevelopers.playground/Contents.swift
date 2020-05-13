737c8af5d052e33c3036b3c345fc6122618c4d7f
Matt Martindale <martindalematt18@yahoo.com>
2020-05-12 19:49:49 -0600

    Attempting first run of program

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,29 +1,42 @@
 import UIKit
 
 class Spoon {
+    
     func pickUp() {
-        lock.lock()
-        putDown()
+        semaphoreSpoon.wait()
     }
     
     func putDown() {
-        
-        lock.unlock()
+        semaphoreSpoon.signal()
     }
     
-    private let lock = NSLock()
+    private let semaphoreSpoon = DispatchSemaphore(value: 0)
 }
 
 class Developer {
-    var leftSpoon: Bool
-    var rightSpoon: Bool
+    var hasLeftSpoon: Spoon
+    var hasRightSpoon: Spoon
+    
+    init(leftHand: Spoon, rightHand: Spoon) {
+        self.hasLeftSpoon = leftHand
+        self.hasRightSpoon = rightHand
+    }
     
     func think() {
-        Spoon.pickUp(Spoon.init())
+        hasLeftSpoon.pickUp()
+        "Picked up left spoon"
+        hasRightSpoon.pickUp()
+        "Picked up right spoon"
     }
     
     func eat() {
+        let randomTime = Int.random(in: 1...5)
+        usleep(useconds_t(randomTime))
         
+        print("Developer is eating")
+        
+        hasLeftSpoon.putDown()
+        hasRightSpoon.putDown()
     }
     
     func run() {
@@ -31,9 +44,23 @@ class Developer {
         eat()
         run()
     }
-    
-    init(leftSpoon: Bool, rightSpoon: Bool) {
-        self.leftSpoon = leftSpoon
-        self.rightSpoon = rightSpoon
-    }
 }
+
+let spoon1 = Spoon()
+let spoon2 = Spoon()
+let spoon3 = Spoon()
+let spoon4 = Spoon()
+let spoon5 = Spoon()
+
+let dev1 = Developer(leftHand: spoon1, rightHand: spoon2)
+let dev2 = Developer(leftHand: spoon2, rightHand: spoon3)
+let dev3 = Developer(leftHand: spoon3, rightHand: spoon4)
+let dev4 = Developer(leftHand: spoon4, rightHand: spoon5)
+let dev5 = Developer(leftHand: spoon5, rightHand: spoon1)
+
+let devArray: [Developer] = [dev1, dev2, dev3, dev4, dev5]
+
+DispatchQueue.concurrentPerform(iterations: 5) {
+devArray[$0].run()
+}
+

412a049ed10d7804e0dc79877b938717e0ee2df5
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-05-13 01:24:23 -0400

    I have all the print statements and logic implemented, something isn't right tho because devs are able to pick up the same spoon at the same time.

--- HungryDevelopers/HungryDevelopers/ViewController.swift
+++ HungryDevelopers/HungryDevelopers/ViewController.swift
@@ -9,94 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //MARK: - The spoons -
         
-        class Spoon {
-            
-            //MARK: - Properties -
-            
-            private let lock = NSLock()
-            
-            //MARK: - Methods -
-            
-            func pickUp() {
-                lock.lock()
-                print("The spoon was picked up")
-            }
-            
-            func putDown() {
-                lock.unlock()
-                print("The spoon was put down")
-            }
-            
+        let spoon1 = Spoon(name: "spoon 1", index: 1)
+        let spoon2 = Spoon(name: "spoon 2", index: 2)
+        let spoon3 = Spoon(name: "spoon 3", index: 3)
+        let spoon4 = Spoon(name: "spoon 4", index: 4)
+        let spoon5 = Spoon(name: "spoon 5", index: 5)
+        
+        //MARK: - The developers -
+        
+        let dev1 = Developer(name: "dev 1", leftSpoon: spoon1, rightSpoon: spoon5)
+        let dev2 = Developer(name: "dev 2", leftSpoon: spoon2, rightSpoon: spoon1)
+        let dev3 = Developer(name: "dev 3", leftSpoon: spoon3, rightSpoon: spoon2)
+        let dev4 = Developer(name: "dev 4", leftSpoon: spoon4, rightSpoon: spoon3)
+        let dev5 = Developer(name: "dev 5", leftSpoon: spoon5, rightSpoon: spoon4)
+        
+        var devs: [Developer] {
+            var developers: [Developer] = []
+            developers.append(dev1)
+            developers.append(dev2)
+            developers.append(dev3)
+            developers.append(dev4)
+            developers.append(dev5)
+            return developers
         }
-
-        class Developer {
-            
-            //MARK: - Properties -
-            
-            let leftSpoon = Spoon()
-            let rightSpoon = Spoon()
-            
-            //MARK: - Mehtods -
-            
-            func think() {
-                
-                leftSpoon.pickUp()
-                rightSpoon.pickUp()
-                
-            }
-            
-            func eat() {
-                
-                usleep(1)
-                rightSpoon.putDown()
-                leftSpoon.putDown()
-                
-            }
-            
-            func run() {
-                
-                while 1 == 1 {
-                    think()
-                    eat()
-                }
-                
-            }
-            
-        }
-
-        let developer1 = Developer()
-        let developer2 = Developer()
-        let developer3 = Developer()
-        let developer4 = Developer()
-        let developer5 = Developer()
-
-        let spoon1 = Spoon()
-        let spoon2 = Spoon()
-        let spoon3 = Spoon()
-        let spoon4 = Spoon()
-        let spoon5 = Spoon()
-
-        var developers: [Developer] = []
-
-        developers.append(developer1)
-        developers.append(developer2)
-        developers.append(developer3)
-        developers.append(developer4)
-        developers.append(developer5)
-
+        
+        //MARK: - Let's Eat! -
+        
         DispatchQueue.concurrentPerform(iterations: 5) {
-        developers[$0].run()
+            devs[$0].run()
         }
-
         
-        
-    }
-
-
+    } //End of the ViewDidLoad method
+    
+    
 }
 

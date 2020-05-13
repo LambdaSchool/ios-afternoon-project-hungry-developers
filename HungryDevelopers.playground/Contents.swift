d4241cb81541e92bf6f9ef5ea7558e4349584d0d
Matt Martindale <martindalematt18@yahoo.com>
2020-05-12 20:29:52 -0600

    MVP

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class Spoon {
     
+    public let rank: Int
+    
     func pickUp() {
         spoonLock.lock()
     }
@@ -11,40 +13,52 @@ class Spoon {
     }
     
     private let spoonLock = NSLock()
+    
+    init(rank: Int) {
+        self.rank = rank
+    }
 }
 
 class Developer {
     var devIndex: Int
     
-    var hasLeftSpoon: Spoon
-    var hasRightSpoon: Spoon
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
     
     init(leftHand: Spoon, rightHand: Spoon, devIndex: Int) {
-        self.hasLeftSpoon = leftHand
-        self.hasRightSpoon = rightHand
+        self.leftSpoon = leftHand
+        self.rightSpoon = rightHand
         self.devIndex = devIndex
     }
     
     func think() {
-        hasLeftSpoon.pickUp()
-        print("Dev \(devIndex): Picked up left spoon")
-        hasRightSpoon.pickUp()
-        print("Dev \(devIndex): Picked up right spoon")
-        eat()
+        
+        if leftSpoon.rank < rightSpoon.rank {
+            leftSpoon.pickUp()
+            print("Dev \(devIndex): Picked up left spoon #\(leftSpoon.rank)")
+            rightSpoon.pickUp()
+            print("Dev \(devIndex): Picked up right spoon #\(rightSpoon.rank)")
+            eat()
+        } else {
+            rightSpoon.pickUp()
+            print("Dev \(devIndex): Picked up right spoon #\(rightSpoon.rank)")
+        }
     }
     
     func eat() {
         let randomTime = Int.random(in: 1...100000)
-        usleep(useconds_t(randomTime))
         
         print("Developer \(devIndex): is eating")
-        
-        hasLeftSpoon.putDown()
-        print("Dev \(devIndex): put down left spoon")
-        hasRightSpoon.putDown()
-        print("Dev \(devIndex): put down right spoon")
-        
+        usleep(useconds_t(randomTime))
+        print("Dev \(devIndex): Ate for \(randomTime) milli-secs")
         print("Developer \(devIndex): has finished eating")
+        
+        leftSpoon.putDown()
+        print("Dev \(devIndex): put down left spoon #\(leftSpoon.rank)")
+        rightSpoon.putDown()
+        print("Dev \(devIndex): put down right spoon #\(rightSpoon.rank)")
+        
+        
     }
     
     func run() {
@@ -53,11 +67,11 @@ class Developer {
     }
 }
 
-let spoon1 = Spoon()
-let spoon2 = Spoon()
-let spoon3 = Spoon()
-let spoon4 = Spoon()
-let spoon5 = Spoon()
+let spoon1 = Spoon(rank: 1)
+let spoon2 = Spoon(rank: 2)
+let spoon3 = Spoon(rank: 3)
+let spoon4 = Spoon(rank: 4)
+let spoon5 = Spoon(rank: 5)
 
 let dev1 = Developer(leftHand: spoon1, rightHand: spoon2, devIndex: 1)
 let dev2 = Developer(leftHand: spoon2, rightHand: spoon3, devIndex: 2)
@@ -68,6 +82,7 @@ let dev5 = Developer(leftHand: spoon5, rightHand: spoon1, devIndex: 5)
 let devArray: [Developer] = [dev1, dev2, dev3, dev4, dev5]
 
 DispatchQueue.concurrentPerform(iterations: 5) {
-devArray[$0].run()
+    print($0)
+    devArray[$0].run()
 }
 

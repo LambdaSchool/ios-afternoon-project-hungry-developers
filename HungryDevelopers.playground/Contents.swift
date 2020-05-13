ddbfca7348cecd97b4c9278c4b5046313e90f3b7
Matt Martindale <martindalematt18@yahoo.com>
2020-05-12 20:03:01 -0600

    program works but dev 5 never gets to eat

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -3,45 +3,52 @@ import UIKit
 class Spoon {
     
     func pickUp() {
-        semaphoreSpoon.wait()
+        spoonLock.lock()
     }
     
     func putDown() {
-        semaphoreSpoon.signal()
+        spoonLock.unlock()
     }
     
-    private let semaphoreSpoon = DispatchSemaphore(value: 0)
+    private let spoonLock = NSLock()
 }
 
 class Developer {
+    var devIndex: Int
+    
     var hasLeftSpoon: Spoon
     var hasRightSpoon: Spoon
     
-    init(leftHand: Spoon, rightHand: Spoon) {
+    init(leftHand: Spoon, rightHand: Spoon, devIndex: Int) {
         self.hasLeftSpoon = leftHand
         self.hasRightSpoon = rightHand
+        self.devIndex = devIndex
     }
     
     func think() {
         hasLeftSpoon.pickUp()
-        "Picked up left spoon"
+        print("Dev \(devIndex): Picked up left spoon")
         hasRightSpoon.pickUp()
-        "Picked up right spoon"
+        print("Dev \(devIndex): Picked up right spoon")
+        eat()
     }
     
     func eat() {
-        let randomTime = Int.random(in: 1...5)
+        let randomTime = Int.random(in: 1...100000)
         usleep(useconds_t(randomTime))
         
-        print("Developer is eating")
+        print("Developer \(devIndex): is eating")
         
         hasLeftSpoon.putDown()
+        print("Dev \(devIndex): put down left spoon")
         hasRightSpoon.putDown()
+        print("Dev \(devIndex): put down right spoon")
+        
+        print("Developer \(devIndex): has finished eating")
     }
     
     func run() {
         think()
-        eat()
         run()
     }
 }
@@ -52,11 +59,11 @@ let spoon3 = Spoon()
 let spoon4 = Spoon()
 let spoon5 = Spoon()
 
-let dev1 = Developer(leftHand: spoon1, rightHand: spoon2)
-let dev2 = Developer(leftHand: spoon2, rightHand: spoon3)
-let dev3 = Developer(leftHand: spoon3, rightHand: spoon4)
-let dev4 = Developer(leftHand: spoon4, rightHand: spoon5)
-let dev5 = Developer(leftHand: spoon5, rightHand: spoon1)
+let dev1 = Developer(leftHand: spoon1, rightHand: spoon2, devIndex: 1)
+let dev2 = Developer(leftHand: spoon2, rightHand: spoon3, devIndex: 2)
+let dev3 = Developer(leftHand: spoon3, rightHand: spoon4, devIndex: 3)
+let dev4 = Developer(leftHand: spoon4, rightHand: spoon5, devIndex: 4)
+let dev5 = Developer(leftHand: spoon5, rightHand: spoon1, devIndex: 5)
 
 let devArray: [Developer] = [dev1, dev2, dev3, dev4, dev5]
 

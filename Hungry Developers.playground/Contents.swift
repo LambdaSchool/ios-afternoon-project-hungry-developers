b61c083786ce045f5cb40c7c206a99df76efd90f
John Holowesko <jholowesko@gmail.com>
2020-05-13 15:52:30 -0400

    Logic fixes for Part 4

--- Hungry Developers.playground/Contents.swift
+++ Hungry Developers.playground/Contents.swift
@@ -1,22 +1,32 @@
 import Foundation
 
 
-// MARK: - Spoon
+// MARK: - Spoon (Class)
 
 class Spoon {
     
+    /// Properties
+    let index: Int
     let lock = NSLock()
-    var hasSpoon: Bool = false
+    var inPossession: Bool = false
     
+    /// Initializer
+    init(index: Int) {
+        self.index = index
+    }
+    
+    /// Functions
+    // Pickup
     func pickUp() {
-        while hasSpoon == false {
-            hasSpoon = lock.try()
+        while inPossession == false {
+            inPossession = lock.try()
         }
     }
     
+    // Putdown
     func putdown() {
         lock.unlock()
-        hasSpoon = false
+        inPossession = false
     }
 }
 
@@ -30,30 +40,27 @@ class Developer {
     let rightSpoon: Spoon
     
     init(id: Int, leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.id = id
         self.leftSpoon = leftSpoon
         self.rightSpoon = rightSpoon
-        self.id = id
     }
     
     func think(completion: @escaping () -> ()) {
-        DispatchQueue.global().async {
-            self.leftSpoon.pickUp()
-            print("Picked up Left Spoon!")
-            self.rightSpoon.pickUp()
-            print("Picked up Right Spoon!")
-            print("Got Both!")
-            completion()
-        }
+        var spoonsArray: [Spoon] = [leftSpoon, rightSpoon]
+        spoonsArray.sort()
+        spoonsArray[0].pickUp()
+        print("Picked up Spoon: \(spoonsArray[0].index)")
+        spoonsArray[1].pickUp()
+        print("Picked up Spoon: \(spoonsArray[1].index)")
+        completion()
     }
     
     func eat() {
-        print("Eating")
+        print("Dev:\(self.id) Eating")
         usleep(1000000)
         leftSpoon.putdown()
-        print("Put Down Left")
         rightSpoon.putdown()
-        print("Put Down Right")
-        print("Dropped Both")
+        print("Dev:\(self.id) Dropped Both Spoons")
     }
     
     func run() {
@@ -66,11 +73,12 @@ class Developer {
 
 // Create 5 Spoons and 5 Developers.
 
-let spoon1 = Spoon()
-let spoon2 = Spoon()
-let spoon3 = Spoon()
-let spoon4 = Spoon()
-let spoon5 = Spoon()
+let spoon1 = Spoon(index: 1)
+let spoon2 = Spoon(index: 2)
+let spoon3 = Spoon(index: 3)
+let spoon4 = Spoon(index: 4)
+let spoon5 = Spoon(index: 5)
+
 let dev1 = Developer(id: 1, leftSpoon: spoon1, rightSpoon: spoon2)
 let dev2 = Developer(id: 2, leftSpoon: spoon2, rightSpoon: spoon3)
 let dev3 = Developer(id: 3, leftSpoon: spoon3, rightSpoon: spoon4)
@@ -82,4 +90,15 @@ let devs: [Developer] = [dev1, dev2, dev3, dev4, dev5]
 
 DispatchQueue.concurrentPerform(iterations: devs.count) { (index) in
     devs[index].run()
+}
+
+
+extension Spoon: Comparable {
+    static func < (lhs: Spoon, rhs: Spoon) -> Bool {
+        lhs.index < rhs.index
+    }
+    
+    static func == (lhs: Spoon, rhs: Spoon) -> Bool {
+        lhs.index == rhs.index
+    }
 }

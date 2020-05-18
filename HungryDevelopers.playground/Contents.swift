03814c3a15989dd66cef4709a6f5a908e39f118a
kensam1 <samarakodykennedy@gmail.com>
2020-05-17 21:58:16 -0400

    Finished Hungry Developers

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -3,8 +3,15 @@ import UIKit
 
 class Spoon {
     
+    var index: Int
+    
+    init(index: Int) {
+        self.index = index
+    }
+    
     private var lock = NSLock()
     func pickUp() {
+        print("picked up spoon")
         lock.lock()
         putDown()
         
@@ -14,6 +21,7 @@ class Spoon {
     }
     
     func putDown(){
+        print("put down spoon")
         
     }
     
@@ -22,23 +30,35 @@ class Spoon {
 class Developer {
     let leftSpoon: Spoon
     let rightSpoon: Spoon
+    let name: String
     
-    init(leftSpoon: Spoon, rightSpoon: Spoon){
+    init(name: String, leftSpoon: Spoon, rightSpoon: Spoon){
         self.leftSpoon = leftSpoon
         self.rightSpoon = rightSpoon
+        self.name = name
     }
     
     func think(){
-        leftSpoon.pickUp()
-        rightSpoon.pickUp()
+        
+        while leftSpoon.index < rightSpoon.index {
+            print("\(name) think picking up left spoon")
+            leftSpoon.pickUp()
+            print("\(name) think picking up right spoon")
+            rightSpoon.pickUp()
+            print("picked up right spoon")
+        }
+        
         
         
     }
     
     func eat(){
+        print("\(name) is eating")
         usleep(10000)
         leftSpoon.putDown()
+        print("\(name) put down left spoon")
         rightSpoon.putDown()
+        print("\(name) put down right spoon")
         
     }
     
@@ -50,17 +70,17 @@ class Developer {
     
 }
 
-let spoon1 = Spoon()
-let spoon2 = Spoon()
-let spoon3 = Spoon()
-let spoon4 = Spoon()
-let spoon5 = Spoon()
+let spoon1 = Spoon(index: 1)
+let spoon2 = Spoon(index: 2)
+let spoon3 = Spoon(index: 3)
+let spoon4 = Spoon(index: 4)
+let spoon5 = Spoon(index: 5)
 
-let dev1 = Developer(leftSpoon: spoon1, rightSpoon: spoon2)
-let dev2 = Developer(leftSpoon: spoon2, rightSpoon: spoon3)
-let dev3 = Developer(leftSpoon: spoon3, rightSpoon: spoon4)
-let dev4 = Developer(leftSpoon: spoon4, rightSpoon: spoon5)
-let dev5 = Developer(leftSpoon: spoon5, rightSpoon: spoon1)
+let dev1 = Developer(name: "dev1", leftSpoon: spoon1, rightSpoon: spoon2)
+let dev2 = Developer(name: "dev2", leftSpoon: spoon2, rightSpoon: spoon3)
+let dev3 = Developer(name: "dev3", leftSpoon: spoon3, rightSpoon: spoon4)
+let dev4 = Developer(name: "dev4", leftSpoon: spoon4, rightSpoon: spoon5)
+let dev5 = Developer(name: "dev5", leftSpoon: spoon5, rightSpoon: spoon1)
 
 let dev = [dev1, dev2, dev3, dev4, dev5]
 

6c5fe2901088fdeab0d5d6a98bdf24571931a888
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-06-03 15:14:36 -0700

    Created Project and Files

---
+++ HungryDevelopers/HungryDevelopers/Classes/Developer.swift
@@ -0,0 +1,9 @@
+//
+//  Developer.swift
+//  HungryDevelopers
+//
+//  Created by Enzo Jimenez-Soto on 6/3/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import Foundation
---
+++ HungryDevelopers/HungryDevelopers/Classes/Spoons.swift
@@ -0,0 +1,16 @@
+//
+//  Spoons.swift
+//  HungryDevelopers
+//
+//  Created by Enzo Jimenez-Soto on 6/3/20.
+//  Copyright © 2020 Enzo Jimenez-Soto. All rights reserved.
+//
+
+import Foundation
+
+
+class Spoon {
+    let identifier: Int
+    
+    var isPickedUp: Bool = false
+}

6e5c396d16a0f18bcc7a967a3f0e450b3359fede
Cody Morley <codymorleyinc@gmail.com>
2020-06-03 13:28:08 -0700

    outline done.

---
+++ HungryDevelopers.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class Spoon {
+    
+    func pickUp(){
+        
+    }
+    
+    func putDown(){
+        
+    }
+}
+
+
+
+class Developer {
+    
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    
+    init(_ leftSpoon: Spoon, _ rightSpoon: Spoon) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+    }
+    
+    func think(){
+        
+    }
+    
+    func eat(){
+        
+    }
+    
+    func run(){
+        
+    }
+}
+
+
+//Spoons
+
+
+
+//Devs

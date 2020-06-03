53cfbe227f1212ec4aa9ccdbfb5a6265d36df41c
Stephanie Ballard <user.name>
2020-06-03 16:42:10 -0700

    .

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -3,27 +3,38 @@ import UIKit
 
 class Spoon {
     
-    private let lock = NSLock()
+    let id: Int
+    private var spoonLock = NSLock()
+    var whichDevIsEating: UUID?
     
-    enum Spoons {
-        case leftSpoon
-        case rightSpoon
+    init(_ id: Int) {
+        self.id = id
     }
     
-    func pickUp(spoon1: Spoons, spoon2: Spoons) {
-        
+    func pickUp() {
+        spoonLock.lock()
     }
     
     func putDown() {
-        
+        spoonLock.unlock()
     }
 }
 
-class Developer: Spoon {
+class Developer {
     
+    let id = UUID()
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    var name: String
+    
+    init(_ leftSpoon: Spoon, _ rightSpoon: Spoon, name: String) {
+        self.leftSpoon = leftSpoon
+        self.rightSpoon = rightSpoon
+        self.name = name
+    }
     
     func think() {
-        pickUp(spoon1: Spoons.leftSpoon, spoon2: Spoons.rightSpoon)
+        pickUp()
     }
     
     func eat() {
@@ -31,8 +42,7 @@ class Developer: Spoon {
     }
     
     func run() {
-        let x = 0
-        while x == 0 {
+        while true {
             think()
             eat()
         }
@@ -40,5 +50,20 @@ class Developer: Spoon {
     
 }
 
+let spoon1 = Spoon(1)
+let spoon2 = Spoon(2)
+let spoon3 = Spoon(3)
+let spoon4 = Spoon(4)
+let spoon5 = Spoon(5)
 
-let developers = ["Developer1", "Developer2", "Developer3", "Developer4", "Developer5"]
+let developer1 =
+let developer2 =
+let developer3 =
+let developer4 =
+let developer5 =
+
+let developers = [developer1, developer2, developer3, developer4, developer5]
+
+DispatchQueue.concurrentPerform(iterations: 5) {
+developers[$0].run()
+}

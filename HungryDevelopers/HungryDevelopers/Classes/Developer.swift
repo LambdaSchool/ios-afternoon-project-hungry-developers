d137f8a737bf6d8a786fc9b4184dedc77d0a32eb
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-06-03 15:40:54 -0700

    MVP?

--- HungryDevelopers/HungryDevelopers/Classes/Developer.swift
+++ HungryDevelopers/HungryDevelopers/Classes/Developer.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+
+
+class Developer {
+    
+    var name: String
+    var leftSpoon: Spoon
+    var rightSpoon: Spoon
+    
+    var isEating = true
+    
+    init(name: String, leftSpoon: Spoon, rightSpoon: Spoon) {
+        self.name = name
+        self.rightSpoon = rightSpoon
+        self.leftSpoon = leftSpoon
+    }
+    
+    func think() {
+        
+        if leftSpoon.identifier < rightSpoon.identifier {
+            leftSpoon.pickUp()
+            print("\(name) picked up left spoon")
+            rightSpoon.pickUp()
+            print("\(name) picked up right spoon")
+        } else {
+            rightSpoon.pickUp()
+            print("\(name) picked up right spoon")
+            leftSpoon.pickUp()
+            print("\(name) picked up left spoon")
+        }
+    }
+    func eat() {
+        print("\(name) starts eating")
+        usleep(1000000)
+        leftSpoon.putDown()
+        print("\(name) puts down left spoon")
+        rightSpoon.putDown()
+        print("\(name) puts down right spoon")
+        
+        isEating = !isEating
+    }
+    
+    func run() {
+        print("Running...")
+        let isTrue = true
+        if isTrue == true {
+            think()
+            eat()
+            run()
+        }
+    }
+}
--- HungryDevelopers/HungryDevelopers/ViewControllers/ViewController.swift
+++ HungryDevelopers/HungryDevelopers/ViewControllers/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dev1Label: UILabel!
+    @IBOutlet weak var dev2Label: UILabel!
+    @IBOutlet weak var dev3Label: UILabel!
+    @IBOutlet weak var dev4Label: UILabel!
+    @IBOutlet weak var dev5Label: UILabel!
+    
+    @IBOutlet weak var spoon1Label: UILabel!
+    @IBOutlet weak var spoon2Label: UILabel!
+    @IBOutlet weak var spoon3Label: UILabel!
+    @IBOutlet weak var spoon4Label: UILabel!
+    @IBOutlet weak var spoon5Label: UILabel!
+    
+    
+    let dev1 = Developer(name: "Dev1", leftSpoon: Spoon(identifier: 5), rightSpoon: Spoon(identifier: 1))
+    let dev2 = Developer(name: "Dev2", leftSpoon: Spoon(identifier: 1), rightSpoon: Spoon(identifier: 2))
+    let dev3 = Developer(name: "Dev3", leftSpoon: Spoon(identifier: 2), rightSpoon: Spoon(identifier: 3))
+    let dev4 = Developer(name: "Dev4", leftSpoon: Spoon(identifier: 3), rightSpoon: Spoon(identifier: 4))
+    let dev5 = Developer(name: "Dev5", leftSpoon: Spoon(identifier: 4), rightSpoon: Spoon(identifier: 5))
+    
+    var developers: [Developer] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        [dev1, dev2, dev3, dev4, dev5].forEach { index in
+            developers.append(index)
+        }
+        
+        DispatchQueue.concurrentPerform(iterations: 5) {
+            developers[$0].run()
+        }
     }
-
-
 }
 

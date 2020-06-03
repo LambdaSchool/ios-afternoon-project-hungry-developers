6228f4e241aae5c9eac07e0e7301ec55f02b6578
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-06-03 15:17:44 -0700

    Finsihed Spoons class

--- HungryDevelopers/HungryDevelopers/Classes/Spoons.swift
+++ HungryDevelopers/HungryDevelopers/Classes/Spoons.swift
@@ -13,4 +13,20 @@ class Spoon {
     let identifier: Int
     
     var isPickedUp: Bool = false
+    init(identifier: Int) {
+        self.identifier = identifier
+    }
+    
+    private var lock = NSLock()
+    
+    func pickUp() {
+        lock.lock()
+        isPickedUp = true
+    }
+    
+    func putDown() {
+        lock.unlock()
+        isPickedUp = false
+    }
+    
 }

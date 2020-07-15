5f66ccff536534184a3f627fc72b06024ba858f7
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-14 18:53:18 -0700

    Stubbed out Classes

--- HungryDevelopers.playground/Contents.swift
+++ HungryDevelopers.playground/Contents.swift
@@ -1,1 +1,31 @@
 import Foundation
+
+// MARK: - Classes
+
+class Spoon {
+
+    func pickup(){
+        
+    }
+    
+    func putDown(){
+        
+    }
+}
+
+class Developer {
+//    var leftSpoon
+//    var rightSpoon
+    
+    func think(){
+        
+    }
+    
+    func eat(){
+        
+    }
+    
+    func run(){
+        
+    }
+}

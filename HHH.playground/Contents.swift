5d37c134bfe7712c3129bdc5e78191c6f4ac3f1b
Ronald Huston Jr <rghuston9@gmail.com>
2020-09-08 22:20:24 -0500

    part four

--- HHH.playground/Contents.swift
+++ HHH.playground/Contents.swift
@@ -2,13 +2,18 @@ import Foundation
 
 class Spoon {
     private let lock = NSLock()
+    var index: Int
      
     func pickUp() {
-        
+        lock.lock()
     }
     
     func putDown() {
-        
+        lock.unlock()
+    }
+    
+    init(index: Int) {
+        self.index = index
     }
 }
 
@@ -17,23 +22,27 @@ class Developer {
     var leftSpoon: Spoon
     var rightSpoon: Spoon
     var randomTime = UInt32.random(in: 5...10)
-    var devCall: String
+    var name: String
     
     func think() {
-        leftSpoon.pickUp()
-        rightSpoon.pickUp()
+        if rightSpoon.index > leftSpoon.index {
+            leftSpoon.pickUp()
+        } else {
+            rightSpoon.pickUp()
+        }
+        
         return
     }
     
     func eat() {
-        print("\(devCall) is eating")
+        print("\(name) is eating")
         
         usleep(randomTime)
         leftSpoon.putDown()
-        print("\(devCall) put down their left spoon.")
+        print("\(name) put down their left spoon.")
         
         rightSpoon.putDown()
-        print("\(devCall) put down their right spoon.")
+        print("\(name) put down their right spoon.")
     }
     
     func run() {
@@ -42,24 +51,24 @@ class Developer {
             eat()
         }
     }
-    init(leftSppon: Spoon, rightSpoon: Spoon, devCall: String) {
+    init(leftSppon: Spoon, rightSpoon: Spoon, name: String) {
         self.leftSpoon = leftSppon
         self.rightSpoon = rightSpoon
-        self.devCall = devCall
+        self.name = name
     }
 }
 
-let spoonI = Spoon()
-let spoonII = Spoon()
-let spoonIII = Spoon()
-let spoonIV = Spoon()
-let spoonV = Spoon()
+let spoonI = Spoon(index: 1)
+let spoonII = Spoon(index: 2)
+let spoonIII = Spoon(index: 3)
+let spoonIV = Spoon(index: 4)
+let spoonV = Spoon(index: 5)
 
-let devA = Developer(leftSppon: spoonI, rightSpoon: spoonII, devCall: "devA")
-let devB = Developer(leftSppon: spoonII, rightSpoon: spoonIII, devCall: "devB")
-let devC = Developer(leftSppon: spoonIII, rightSpoon: spoonIV, devCall: "devC")
-let devD = Developer(leftSppon: spoonIV, rightSpoon: spoonV, devCall: "devD")
-let devE = Developer(leftSppon: spoonV, rightSpoon: spoonI, devCall: "devE")
+let devA = Developer(leftSppon: spoonI, rightSpoon: spoonII, name: "david")
+let devB = Developer(leftSppon: spoonII, rightSpoon: spoonIII, name: "brando")
+let devC = Developer(leftSppon: spoonIII, rightSpoon: spoonIV, name: "daniel")
+let devD = Developer(leftSppon: spoonIV, rightSpoon: spoonV, name: "ronald")
+let devE = Developer(leftSppon: spoonV, rightSpoon: spoonI, name: "henry")
 let developers = [devA, devB, devC, devD, devE]
 
 DispatchQueue.concurrentPerform(iterations: 5) {

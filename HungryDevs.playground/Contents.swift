5d9a510b11941abb7b812a064485226a1084712a
BrysonSaclausa <brysonSaclausa@Owners-MacBook-Pro.local>
2020-09-08 23:57:02 -0700

    stubbed out classes

---
+++ HungryDevs.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class Spoon {
+    
+    func pickUp() {
+        
+    }
+    
+    func putDown() {
+        
+    }
+}
+
+
+class Developer {
+    var leftSpoon: Spoon?
+    var rightSpoon: Spoon?
+    
+    func think() {
+        
+        return
+    }
+    
+    func eat() {
+        
+        
+    }
+    
+    func run() {
+       
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}

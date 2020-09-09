a9fb4fcad289e351852b3bfde9dc2bf362af95bf
Ronald Huston Jr <rghuston9@gmail.com>
2020-09-08 21:48:49 -0500

    part two

--- HHH.playground/Contents.swift
+++ HHH.playground/Contents.swift
@@ -17,6 +17,7 @@ class Developer {
     var leftSpoon: Spoon
     var rightSpoon: Spoon
     var randomTime = UInt32.random(in: 5...10)
+    var devCall: String
     
     func think() {
         leftSpoon.pickUp()
@@ -25,9 +26,14 @@ class Developer {
     }
     
     func eat() {
+        print("\(devCall) is eating")
+        
         usleep(randomTime)
         leftSpoon.putDown()
+        print("\(devCall) put down their left spoon.")
+        
         rightSpoon.putDown()
+        print("\(devCall) put down their right spoon.")
     }
     
     func run() {
@@ -36,9 +42,10 @@ class Developer {
             eat()
         }
     }
-    init(leftSppon: Spoon, rightSpoon: Spoon) {
+    init(leftSppon: Spoon, rightSpoon: Spoon, devCall: String) {
         self.leftSpoon = leftSppon
         self.rightSpoon = rightSpoon
+        self.devCall = devCall
     }
 }
 
@@ -48,11 +55,11 @@ let spoonIII = Spoon()
 let spoonIV = Spoon()
 let spoonV = Spoon()
 
-let devA = Developer(leftSppon: spoonI, rightSpoon: spoonII)
-let devB = Developer(leftSppon: spoonII, rightSpoon: spoonIII)
-let devC = Developer(leftSppon: spoonIII, rightSpoon: spoonIV)
-let devD = Developer(leftSppon: spoonIV, rightSpoon: spoonV)
-let devE = Developer(leftSppon: spoonV, rightSpoon: spoonI)
+let devA = Developer(leftSppon: spoonI, rightSpoon: spoonII, devCall: "devA")
+let devB = Developer(leftSppon: spoonII, rightSpoon: spoonIII, devCall: "devB")
+let devC = Developer(leftSppon: spoonIII, rightSpoon: spoonIV, devCall: "devC")
+let devD = Developer(leftSppon: spoonIV, rightSpoon: spoonV, devCall: "devD")
+let devE = Developer(leftSppon: spoonV, rightSpoon: spoonI, devCall: "devE")
 let developers = [devA, devB, devC, devD, devE]
 
 DispatchQueue.concurrentPerform(iterations: 5) {
